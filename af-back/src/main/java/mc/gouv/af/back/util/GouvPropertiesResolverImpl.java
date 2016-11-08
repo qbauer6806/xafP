1de6df24b2613eef516643bdb6e3ce2b74eebf9f
Fabien GAUJOUS <fgaujous@gouv.mc>
2016-11-08 13:28:50 +0000

    #3802 Utilisation des profile pour avoir une implementation différentes de GouvPropertyResolver pour les test
    git-svn-id: http://linuxic/jsrc/af/trunk@39109 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/util/GouvPropertiesResolverImpl.java
+++ af-back/src/main/java/mc/gouv/af/back/util/GouvPropertiesResolverImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,7 @@ import mc.gouv.af.back.service.properties.GouvPropertyNotFoundException;
  *
  */
 @Component
+@Profile("gouv")
 @Transactional(rollbackFor = Exception.class)
 public class GouvPropertiesResolverImpl implements GouvPropertiesResolver {
 

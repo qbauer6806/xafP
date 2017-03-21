443d58afca6319af5229ec41393c916737e1e6f1
Fabien GAUJOUS <fgaujous@gouv.mc>
2017-03-21 09:24:28 +0000

    refs #4669
    git-svn-id: http://linuxic/jsrc/af/trunk@41630 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/config/advice/AfBackControllerAdvice.java
+++ af-back/src/main/java/mc/gouv/af/back/config/advice/AfBackControllerAdvice.java
@@ -1,30 +1,24 @@
 package mc.gouv.af.back.config.advice;
 
-import mc.gouv.Static;
-
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import mc.gouv.af.back.service.properties.GouvPropertiesResolver;
 
 @ControllerAdvice
 public class AfBackControllerAdvice {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AfBackControllerAdvice.class);
 
-    @Value("${application.name}")
-    private String applicationName;
-
-    private String applicationPrefix = StringUtils.EMPTY;
+    @Autowired
+    GouvPropertiesResolver gouvPropertiesResolver;
 
     @ModelAttribute(name = "demarcheId")
     public String addDemarcheId() {
 
-        if (StringUtils.isNotBlank(applicationName)) {
-            applicationPrefix = "." + applicationName;
-        }
-        return Static.getValue("mc.gouv" + applicationPrefix + ".backserver.demarcheId");
+        return gouvPropertiesResolver.getDemarcheId();
     }
 }

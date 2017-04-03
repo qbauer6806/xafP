875ab509c6ae34e4216b4bf4054c5ac3177a3e26
Teddy VERDOYAN <tverdoyan@gouv.mc>
2017-04-03 13:46:03 +0000

    refs #4834 - Motif utilisé désactivé
    git-svn-id: http://linuxic/jsrc/af/trunk@41887 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/util/MotifsCache.java
+++ af-back/src/main/java/mc/gouv/af/back/util/MotifsCache.java
@@ -9,32 +9,27 @@ import mc.gouv.dem.apishared.model.MotifDTO;
  * Composant permettant de gérer un cache des motifs de la démarche courante
  * 
  * @author qdeme
- *
+ * 
  */
 public interface MotifsCache {
 
     /**
-     * Permet de récupérer la liste "cachée" de motifs de la démarche courante
-     * @return
-     */
-    public List<MotifDTO> getMotifs();
-    
-    /**
      * Force le refresh de la liste des motifs depuis le WS puis retourne la liste
+     * 
      * @return
      */
     public List<MotifDTO> fetchMotifs();
-    
-//    /**
-//     * Permet de retourner le motif correspondant à un certain codeMotif, un certain statut, et dans
-//     * la langue souhaitée
-//     * @param codeMotif
-//     * @param statut
-//     * @param langue
-//     * @return
-//     */
-//    public MotifDTO getMotif(String codeMotif, DemandeStatutEnum statut, String langue);
-    
+
+    // /**
+    // * Permet de retourner le motif correspondant à un certain codeMotif, un certain statut, et dans
+    // * la langue souhaitée
+    // * @param codeMotif
+    // * @param statut
+    // * @param langue
+    // * @return
+    // */
+    // public MotifDTO getMotif(String codeMotif, DemandeStatutEnum statut, String langue);
+
     /**
      * Permet de retourner le motif correspondant à un certain codeMotif et dans la langue souhaitée
      * 
@@ -43,20 +38,36 @@ public interface MotifsCache {
      * @return
      */
     public MotifDTO getMotif(String codeMotif, String langue);
-    
+
     /**
      * Permet de retourner tous les motifs d'une certaine langue
+     * 
      * @param langue
      * @return
      */
     public List<MotifDTO> getMotifs(String langue);
-    
+
     /**
      * Permet de retourner tous les motifs d'une certaine langue et d'un certain statut
+     * 
      * @param langue
      * @param statut
      * @return
      */
     public List<MotifDTO> getMotifs(String langue, DemandeStatutEnum statut);
-    
+
+    /**
+     * Permet de récupérer la liste "cachée" de motifs actifs de la démarche courante
+     * 
+     * @return
+     */
+    public List<MotifDTO> getActiveMotifs();
+
+    /**
+     * Permet de récupérer la liste "cachée" de motifs relatifs à la langue et au statut de la demande
+     * 
+     * @return
+     */
+    public List<MotifDTO> getActiveMotifs(String langue, DemandeStatutEnum statut);
+
 }
--- af-back/src/main/java/mc/gouv/af/back/util/MotifsCacheImpl.java
+++ af-back/src/main/java/mc/gouv/af/back/util/MotifsCacheImpl.java
@@ -3,22 +3,22 @@ package mc.gouv.af.back.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import mc.gouv.af.back.service.properties.GouvPropertiesResolver;
+import mc.gouv.dem.apiclient.DemClient;
+import mc.gouv.dem.apishared.model.DemandeStatutEnum;
+import mc.gouv.dem.apishared.model.MotifDTO;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import mc.gouv.af.back.service.properties.GouvPropertiesResolver;
-import mc.gouv.dem.apiclient.DemClient;
-import mc.gouv.dem.apishared.model.DemandeStatutEnum;
-import mc.gouv.dem.apishared.model.MotifDTO;
-
 /**
  * Composant permettant de gérer un cache des motifs de la démarche courante
  * 
  * @author qdeme
- *
+ * 
  */
 @Component
 @Profile("gouv")
@@ -27,6 +27,8 @@ public class MotifsCacheImpl implements MotifsCache {
     private static final Logger LOGGER = LoggerFactory.getLogger(MotifsCacheImpl.class);
 
     private List<MotifDTO> cachedList = new ArrayList<MotifDTO>();
+
+    private List<MotifDTO> cachedActiveList = new ArrayList<MotifDTO>();
 
     private DemClient demClient;
 
@@ -37,7 +39,21 @@ public class MotifsCacheImpl implements MotifsCache {
      * {@inheritDoc}
      */
     @Override
-    public List<MotifDTO> getMotifs() {
+    public List<MotifDTO> getActiveMotifs() {
+
+        // Initialisation si besoin
+        getMotifs();
+
+        // Retour de la liste
+        return cachedActiveList;
+    }
+
+    /**
+     * Retourne les motifs à la fois actifs et inactifs S'assure qu'ils soient initialisés.
+     * 
+     * @return
+     */
+    private List<MotifDTO> getMotifs() {
         // Initialisation du DemClient si pas déjà fait
         ensureInitialized();
 
@@ -45,17 +61,12 @@ public class MotifsCacheImpl implements MotifsCache {
         if (cachedList.size() == 0) {
             LOGGER.info("Récupération des motifs dans DEM...");
             cachedList.addAll(demClient.getMotifs(gouvPropertiesResolver.getDemarcheId()));
-        }
 
-        // Ignorer les motifs désactivés
-        List<MotifDTO> toDelete = new ArrayList<MotifDTO>();
-        for (MotifDTO motif : cachedList) {
-            if (motif.getDateArchive() != null) {
-                toDelete.add(motif);
+            for (MotifDTO motif : cachedList) {
+                if (motif.getDateArchive() == null) {
+                    cachedActiveList.add(motif);
+                }
             }
-        }
-        for (MotifDTO motif : toDelete) {
-            cachedList.remove(motif);
         }
 
         // Retour de la liste
@@ -69,24 +80,10 @@ public class MotifsCacheImpl implements MotifsCache {
     public List<MotifDTO> fetchMotifs() {
         // Vider la liste (forcera getMotifs() à récupérer les nouveaux du WS)
         cachedList.clear();
-
+        cachedActiveList.clear();
         // Retour de la nouvelle liste
         return getMotifs();
     }
-
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public MotifDTO getMotif(String codeMotif, DemandeStatutEnum statut, String langue) {
-    //        List<MotifDTO> motifs = getMotifs();
-    //        for (MotifDTO motif : motifs) {
-    //            if (motif.getCode().equals(codeMotif) && motif.getStatut().equals(statut) && motif.getLangue().equals(langue)) {
-    //                return motif;
-    //            }
-    //        }
-    //        return null;
-    //    }
 
     /**
      * {@inheritDoc}
@@ -99,6 +96,7 @@ public class MotifsCacheImpl implements MotifsCache {
                 return motif;
             }
         }
+
         return null;
     }
 
@@ -114,7 +112,7 @@ public class MotifsCacheImpl implements MotifsCache {
 
     @Override
     public List<MotifDTO> getMotifs(String langue) {
-        List<MotifDTO> motifs = getMotifs();
+        List<MotifDTO> motifs = getActiveMotifs();
         List<MotifDTO> ret = new ArrayList<MotifDTO>();
         for (MotifDTO motif : motifs) {
             if (motif.getLangue().equals(langue)) {
@@ -126,9 +124,19 @@ public class MotifsCacheImpl implements MotifsCache {
 
     @Override
     public List<MotifDTO> getMotifs(String langue, DemandeStatutEnum statut) {
-        List<MotifDTO> motifs = getMotifs();
+        List<MotifDTO> motifs = getActiveMotifs();
         List<MotifDTO> ret = new ArrayList<MotifDTO>();
         for (MotifDTO motif : motifs) {
+            if (motif.getLangue().equals(langue) && motif.getStatut().equals(statut)) {
+                ret.add(motif);
+            }
+        }
+        return ret;
+    }
+
+    public List<MotifDTO> getActiveMotifs(String langue, DemandeStatutEnum statut) {
+        List<MotifDTO> ret = new ArrayList<MotifDTO>();
+        for (MotifDTO motif : cachedActiveList) {
             if (motif.getLangue().equals(langue) && motif.getStatut().equals(statut)) {
                 ret.add(motif);
             }

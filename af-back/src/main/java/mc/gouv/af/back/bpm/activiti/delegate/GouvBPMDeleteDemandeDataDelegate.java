376d22d2b07eac812a800118ef8b5ce4cdda60af
Fabien GAUJOUS <fgaujous@gouv.mc>
2017-11-09 10:01:51 +0000

    refs #6537 - Plus nécessaire de catcher d'exception lors de la suppression d'une data d'une demande
    git-svn-id: http://linuxic/jsrc/af/trunk@44080 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/bpm/activiti/delegate/GouvBPMDeleteDemandeDataDelegate.java
+++ af-back/src/main/java/mc/gouv/af/back/bpm/activiti/delegate/GouvBPMDeleteDemandeDataDelegate.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import mc.gouv.af.back.bpm.GouvBPMException;
 import mc.gouv.af.back.service.properties.GouvPropertiesResolver;
 import mc.gouv.dem.service.DemandesDataService;
-import mc.gouv.xapi.error.exception.client.NotFoundWebException;
 
 @Component
 public class GouvBPMDeleteDemandeDataDelegate implements JavaDelegate {
@@ -23,7 +22,7 @@ public class GouvBPMDeleteDemandeDataDelegate implements JavaDelegate {
 
     @Autowired
     private GouvPropertiesResolver gouvPropertiesResolver;
-    
+
     @Autowired
     private DemandesDataService demandesDataService;
 
@@ -42,13 +41,9 @@ public class GouvBPMDeleteDemandeDataDelegate implements JavaDelegate {
             throw new GouvBPMException("Impossible d'insérer une data avec une clé vide");
         }
 
-        try {
-            //afBackUtils.getDemClient().deleteDemandeData(DEMARCHE_ID, demandeId, dataKeyStr);
-            demandesDataService.deleteDemandeData(gouvPropertiesResolver.getDemarcheId(), demandeId, dataKeyStr);
-        }
-        catch (NotFoundWebException e) {
-            LOGGER.info("Le service retourne que la data n'existait pas (404 : " + e.getErrors().get(0).getLibelle() + ")");
-        }
+        //afBackUtils.getDemClient().deleteDemandeData(DEMARCHE_ID, demandeId, dataKeyStr);
+        demandesDataService.deleteDemandeData(gouvPropertiesResolver.getDemarcheId(), demandeId, dataKeyStr);
+
     }
 
     public Expression getDataKey() {

9c29c755319b4e8ddd9a8a164c281e5958621515
Quentin DEME <qdeme@gouv.mc>
2017-12-18 13:43:45 +0000

    refs #6837 - Dispatcher l'implémentation des caches dans les projets dédiés
    git-svn-id: http://linuxic/jsrc/af/trunk@44437 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/util/PaysCacheImpl.java
+++ af-back/src/main/java/mc/gouv/af/back/util/PaysCacheImpl.java
@@ -3,11 +3,11 @@ package mc.gouv.af.back.util;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import mc.gouv.servicerest.caching.PaysNationalitesCache;
+import mc.gouv.servicerest.caching.PaysNationalitesCacheImpl;
 
 @Profile("gouv")
 @Component
-public class PaysCacheImpl extends PaysNationalitesCache implements PaysCache {
+public class PaysCacheImpl extends PaysNationalitesCacheImpl implements PaysCache {
 
     public PaysCacheImpl(String restUrl, String user, String password, long cacheDuration) {
         super(restUrl, user, password, cacheDuration);
--- af-back/src/main/java/mc/gouv/af/back/util/UtilisateursCacheImpl.java
+++ af-back/src/main/java/mc/gouv/af/back/util/UtilisateursCacheImpl.java
@@ -1,6 +1,6 @@
 package mc.gouv.af.back.util;
 
-public class UtilisateursCacheImpl extends mc.gouv.servicerest.caching.UtilisateursCache implements UtilisateursCache {
+public class UtilisateursCacheImpl extends mc.gouv.logon.caching.UtilisateursCacheImpl implements UtilisateursCache {
 
     public UtilisateursCacheImpl(String codeAppli, long cacheDuration) {
         super(codeAppli, cacheDuration);

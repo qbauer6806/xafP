a10c8877054b3fddc5b4c86a28c3d94e3d549666
Quentin DEME <qdeme@gouv.mc>
2018-05-17 12:44:17 +0000

    Faire en sorte que tout le merdier fonctionne enfin ......
    git-svn-id: http://linuxic/jsrc/af/trunk@45847 f0974e12-e15a-48e7-82bd-60f10596da88

---
+++ af-apiclient/src/main/java/mc/gouv/af/apiclient/TestClass.java
@@ -0,0 +1,18 @@
+package mc.gouv.af.apiclient;
+
+public class TestClass {
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+//        AfApiClient hc = new AfApiClient("http://localhost:9081/hab/api/v1", "abc", "abc");
+//
+//        try {
+//            
+//            hc.annulerDemande(130, 12114);
+//            
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+}

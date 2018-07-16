2366bf63bb16b54c0a5499127939c215ac841cd2
Céline PHAN <cphan.stg@gouv.mc>
2018-07-16 11:58:14 +0000

    Modification du format de date
    git-svn-id: http://linuxic/jsrc/af/trunk@46298 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-backweb/src/main/java/mc/gouv/af/backweb/controller/AbstractController.java
+++ af-backweb/src/main/java/mc/gouv/af/backweb/controller/AbstractController.java
@@ -1,11 +1,16 @@
 package mc.gouv.af.backweb.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,34 +19,41 @@ import mc.gouv.af.back.util.AfBackUtils;
 
 /**
  * 
- * Controller pour gérer entre autre les exceptions survenues sur les pages web et pas les WS
+ * Controller pour gérer entre autre les exceptions survenues sur les pages web
+ * et pas les WS
  * 
  * @author fgaujous
  *
  */
 public abstract class AbstractController extends HandlerInterceptorAdapter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractController.class);
-    
-    @Autowired
-    private AfBackUtils afBackUtils;
+	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractController.class);
 
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleException(Exception ex) {
-        LOGGER.error("Erreur interne", ex);
-        ModelAndView mav = new ModelAndView("error/500");
-        mav.addObject("AfBackUtils", afBackUtils);
-        return mav;
-    }
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		CustomDateEditor editor = new CustomDateEditor(AfBackUtils.sdf_JJ_MM_AAAA, true);
+		binder.registerCustomEditor(Date.class, editor);
+	}
 
-    @ResponseStatus(value = HttpStatus.FORBIDDEN)
-    @ExceptionHandler(AccessDeniedException.class)
-    public ModelAndView handleAccessDeniedException(AccessDeniedException ex) {
-        LOGGER.error("Access Denied", ex);
-        ModelAndView mav = new ModelAndView("error/403");
-        mav.addObject("AfBackUtils", afBackUtils);
-        return mav;
-    } 
+	@Autowired
+	private AfBackUtils afBackUtils;
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleException(Exception ex) {
+		LOGGER.error("Erreur interne", ex);
+		ModelAndView mav = new ModelAndView("error/500");
+		mav.addObject("AfBackUtils", afBackUtils);
+		return mav;
+	}
+
+	@ResponseStatus(value = HttpStatus.FORBIDDEN)
+	@ExceptionHandler(AccessDeniedException.class)
+	public ModelAndView handleAccessDeniedException(AccessDeniedException ex) {
+		LOGGER.error("Access Denied", ex);
+		ModelAndView mav = new ModelAndView("error/403");
+		mav.addObject("AfBackUtils", afBackUtils);
+		return mav;
+	}
 
 }
--- af-backweb/src/main/java/mc/gouv/af/backweb/ws/RechercheDemandesController.java
+++ af-backweb/src/main/java/mc/gouv/af/backweb/ws/RechercheDemandesController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import mc.gouv.af.back.properties.GouvPropertiesResolver;
+import mc.gouv.af.backweb.controller.AbstractController;
 import mc.gouv.dem.service.DemandesService;
 import mc.gouv.dem.service.model.DemandeRechercheDTO;
 import mc.gouv.dem.shared.model.DataRechercheDTO;
@@ -25,45 +26,46 @@ import mc.gouv.dem.shared.model.DemandeDTO;
 
 @RestController
 @RequestMapping("/ws/demandes")
-public class RechercheDemandesController {
+public class RechercheDemandesController extends AbstractController {
 
-    @Autowired
-    private GouvPropertiesResolver gouvPropertiesResolver;
-    
-    @Autowired
-    private DemandesService demandesService;
+	@Autowired
+	private GouvPropertiesResolver gouvPropertiesResolver;
 
-    @RequestMapping(value = "/pageable", method = RequestMethod.GET)
-    public Page<DemandeDTO> getDemandes(@RequestParam(value = "usagerId", required = false) Integer usagerId,
-            @RequestParam(value = "statut", required = false) List<String> statuts,
-            @RequestParam(value = "canal", required = false) List<DemandeCanalEnum> canaux,
-            @RequestParam(value = "agentId", required = false) String agentId,
-            @RequestParam(value = "creationStartDate", required = false) @DateTimeFormat(iso = ISO.DATE) Date creationStartDate,
-            @RequestParam(value = "creationEndDate", required = false) @DateTimeFormat(iso = ISO.DATE) Date creationEndDate,
-            @RequestParam(value = "texte", required = false) String texte,
-            @RequestParam(value = "data", required = false) DataRechercheDTO data, Pageable pageable) {
+	@Autowired
+	private DemandesService demandesService;
 
-        DemandeRechercheDTO demandeRecherche = new DemandeRechercheDTO();
-        demandeRecherche.setDemarcheId(gouvPropertiesResolver.getDemarcheId());
-        demandeRecherche.setAgentAffecteId(agentId);
-        demandeRecherche.setUsagerId(usagerId);
-        demandeRecherche.setCreationStartDate(creationStartDate);
-        demandeRecherche.setCreationEndDate(creationEndDate);
-        demandeRecherche.setStatuts(statuts);
-        demandeRecherche.setTexte(texte);
-        demandeRecherche.setCanaux(canaux);
-        demandeRecherche.setData(data);
-        demandeRecherche.setIdentifiant(null);
-        
-        if (pageable.getSort() != null) {
-            Order order = pageable.getSort().iterator().next();
-            if (order != null) {
-                return demandesService.getDemandes(demandeRecherche, pageable, new String[] {});
-            }
-        }
+	@RequestMapping(value = "/pageable", method = RequestMethod.GET)
+	public Page<DemandeDTO> getDemandes(@RequestParam(value = "usagerId", required = false) Integer usagerId,
+			@RequestParam(value = "statut", required = false) List<String> statuts,
+			@RequestParam(value = "canal", required = false) List<DemandeCanalEnum> canaux,
+			@RequestParam(value = "agentId", required = false) String agentId,
+			@RequestParam(value = "creationStartDate", required = false) @DateTimeFormat(iso = ISO.DATE) Date creationStartDate,
+			@RequestParam(value = "creationEndDate", required = false) @DateTimeFormat(iso = ISO.DATE) Date creationEndDate,
+			@RequestParam(value = "texte", required = false) String texte,
+			@RequestParam(value = "data", required = false) DataRechercheDTO data, Pageable pageable) {
 
-        Pageable newPageable = new PageRequest(pageable.getPageNumber(), pageable.getPageSize(), Sort.Direction.ASC, "identifiant");
-        return demandesService.getDemandes(demandeRecherche, newPageable, new String[] {});
-    }
-    
+		DemandeRechercheDTO demandeRecherche = new DemandeRechercheDTO();
+		demandeRecherche.setDemarcheId(gouvPropertiesResolver.getDemarcheId());
+		demandeRecherche.setAgentAffecteId(agentId);
+		demandeRecherche.setUsagerId(usagerId);
+		demandeRecherche.setCreationStartDate(creationStartDate);
+		demandeRecherche.setCreationEndDate(creationEndDate);
+		demandeRecherche.setStatuts(statuts);
+		demandeRecherche.setTexte(texte);
+		demandeRecherche.setCanaux(canaux);
+		demandeRecherche.setData(data);
+		demandeRecherche.setIdentifiant(null);
+
+		if (pageable.getSort() != null) {
+			Order order = pageable.getSort().iterator().next();
+			if (order != null) {
+				return demandesService.getDemandes(demandeRecherche, pageable, new String[] {});
+			}
+		}
+
+		Pageable newPageable = new PageRequest(pageable.getPageNumber(), pageable.getPageSize(), Sort.Direction.ASC,
+				"identifiant");
+		return demandesService.getDemandes(demandeRecherche, newPageable, new String[] {});
+	}
+
 }

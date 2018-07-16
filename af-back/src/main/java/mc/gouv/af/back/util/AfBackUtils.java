32a949ae0cc82452c40c5974aeca8b4d4195013b
Céline PHAN <cphan.stg@gouv.mc>
2018-07-16 12:00:25 +0000

    Modification du format de date
    git-svn-id: http://linuxic/jsrc/af/trunk@46299 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/main/java/mc/gouv/af/back/util/AfBackUtils.java
+++ af-back/src/main/java/mc/gouv/af/back/util/AfBackUtils.java
@@ -1,5 +1,7 @@
 package mc.gouv.af.back.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -46,264 +48,265 @@ import mc.gouv.servicerest.usager.model.UsagerBean;
 @Component
 public class AfBackUtils {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AfBackUtils.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AfBackUtils.class);
 
-    public static final String MAIL_METADATA_DEMANDEID = "MC_DEMANDEID";
+	public static final String MAIL_METADATA_DEMANDEID = "MC_DEMANDEID";
 
-    public static final String FILE_METADATA_DEMANDEID = "X-MC-DEMANDEID";
+	public static final String FILE_METADATA_DEMANDEID = "X-MC-DEMANDEID";
 
-    public static final String FILE_METADATA_DEMANDESTATUT = "X-MC-DEMANDESTATUT";
+	public static final String FILE_METADATA_DEMANDESTATUT = "X-MC-DEMANDESTATUT";
 
-    private final static String version = AfBackUtils.class.getPackage().getImplementationVersion();
+	private final static String version = AfBackUtils.class.getPackage().getImplementationVersion();
 
-    private static RestTemplate restTemplate;
+	private static RestTemplate restTemplate;
 
-    private static String envName;
+	private static String envName;
 
-    private static String envColor;
+	private static String envColor;
 
-    /**
-     * Version en cache des infos de la démarche
-     */
-    private DemarcheDTO demarche = null;
+	public static DateFormat sdf_JJ_MM_AAAA = new SimpleDateFormat("dd/MM/yyyy");
 
-    @Autowired
-    private GouvPropertiesResolver gouvPropertiesResolver;
+	public static final String MESSAGE_ERREURS_FORMULAIRE = "Le formulaire contient des erreurs.";
 
-    private MailClient mailClient = null;
-    
-    private FileClient fileClient = null;
+	/**
+	 * Version en cache des infos de la démarche
+	 */
+	private DemarcheDTO demarche = null;
 
-    @Autowired
-    private UsagersCache usagersCache;
+	@Autowired
+	private GouvPropertiesResolver gouvPropertiesResolver;
 
-    @Autowired
-    private UtilisateursCache utilisateursCache;
-    
-    @Autowired
-    private DemarchesService demarchesService;
-    
-    @Autowired
-    private DemarchesDataProvider demarchesDataProvider;
+	private MailClient mailClient = null;
 
-    @PostConstruct
-    public void postConstructEnv() {
-        String env = gouvPropertiesResolver.getGouvSharedEnv();
-        // Si production, ne rien afficher
-        if ("prod".equals(env)) {
-            envName = "";
-        } else if ("sup".equals(env)) {
-            envName = "Support";
-        } else if ("pre".equals(env)) {
-            envName = "Pré-production";
-        } else if ("rec".equals(env)) {
-            envName = "Recette";
-        } else if ("dev".equals(env)) {
-            envName = "Développement";
-        } else if ("loc".equals(env)) {
-            envName = "Local";
-        } else {
-            envName = "Environnement inconnu";
-        }
+	private FileClient fileClient = null;
 
-        // Fond noir si environnement de production, et non pas rouge
-        if ("prod".equals(env)) {
-            envColor = "#000000";
-        } else {
-            envColor = gouvPropertiesResolver.getGouvSharedEnvColor();
-        }
-    }
+	@Autowired
+	private UsagersCache usagersCache;
 
-    @PostConstruct
-    public void postConstructRestTemplate() {
-        restTemplate = new RestTemplate();
-        List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
-        MappingJackson2HttpMessageConverter conv = new MappingJackson2HttpMessageConverter();
-        List<MediaType> mediaTypes = new ArrayList<MediaType>();
-        mediaTypes.add(new MediaType("application", "json", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
-        mediaTypes.add(new MediaType("text", "html", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
-        conv.setSupportedMediaTypes(mediaTypes);
-        list.add(conv);
-        restTemplate.setMessageConverters(list);
-    }
+	@Autowired
+	private UtilisateursCache utilisateursCache;
 
-    public static String getVersion() {
-        return version;
-    }
+	@Autowired
+	private DemarchesService demarchesService;
 
-    public static String getAuthenticatedAgentId() {
-        Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (o instanceof User) {
-            return ((User) o).getMatricule();
-        }
+	@Autowired
+	private DemarchesDataProvider demarchesDataProvider;
 
-        return null;
-    }
+	@PostConstruct
+	public void postConstructEnv() {
+		String env = gouvPropertiesResolver.getGouvSharedEnv();
+		// Si production, ne rien afficher
+		if ("prod".equals(env)) {
+			envName = "";
+		} else if ("sup".equals(env)) {
+			envName = "Support";
+		} else if ("pre".equals(env)) {
+			envName = "Pré-production";
+		} else if ("rec".equals(env)) {
+			envName = "Recette";
+		} else if ("dev".equals(env)) {
+			envName = "Développement";
+		} else if ("loc".equals(env)) {
+			envName = "Local";
+		} else {
+			envName = "Environnement inconnu";
+		}
 
-    public static String getAuthenticatedAgentName() {
-        Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (o instanceof User) {
-            return ((User) o).getNom();
-        }
+		// Fond noir si environnement de production, et non pas rouge
+		if ("prod".equals(env)) {
+			envColor = "#000000";
+		} else {
+			envColor = gouvPropertiesResolver.getGouvSharedEnvColor();
+		}
+	}
 
-        return null;
-    }
+	@PostConstruct
+	public void postConstructRestTemplate() {
+		restTemplate = new RestTemplate();
+		List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
+		MappingJackson2HttpMessageConverter conv = new MappingJackson2HttpMessageConverter();
+		List<MediaType> mediaTypes = new ArrayList<MediaType>();
+		mediaTypes.add(new MediaType("application", "json", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
+		mediaTypes.add(new MediaType("text", "html", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
+		conv.setSupportedMediaTypes(mediaTypes);
+		list.add(conv);
+		restTemplate.setMessageConverters(list);
+	}
 
-    /**
-     * Retourne le nom d'un usager à partir de son ID
-     * 
-     * @param usagerId
-     * @return
-     */
-    public String getUsagerNameFromID(Integer usagerId) {
-        UsagerBean u = usagersCache.get(usagerId);
-        if (u == null) {
-            return null;
-        }
-        String prenomNom = StringUtils.EMPTY;
-        if (u.getPrenom() != null) {
-            prenomNom += u.getPrenom();
-        }
-        if (u.getNom() != null) {
-            prenomNom += " " + u.getNom();
-        }
-        return prenomNom;
-    }
+	public static String getVersion() {
+		return version;
+	}
 
-    /**
-     * Retourne le nom d'un utilisateur à partir de son matricule
-     * 
-     * @param userId
-     * @return
-     * @throws RestException
-     */
-    public String getUserNameFromID(String matricule) throws RestException {
-        LOGGER.debug("getUserNameFromID() : Appel à Logon...");
-        User user = utilisateursCache.get(matricule);
-        if (user != null) {
-            return user.getNom();
-        }
-        return null;
-    }
+	public static String getAuthenticatedAgentId() {
+		Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (o instanceof User) {
+			return ((User) o).getMatricule();
+		}
 
-    /**
-     * Génère un UUID version 1 (time+location based UUID)
-     * TODO copié de afservlet, supprimer dans l'un des deux
-     * 
-     * @return
-     */
-    public static UUID generateUUID() {
-        EthernetAddress addr = EthernetAddress.fromInterface();
-        TimeBasedGenerator uuidGenerator = Generators.timeBasedGenerator(addr);
-        UUID uuid = uuidGenerator.generate();
-        return uuid;
-    }
-    
-    public MailClient getMailClient() {
-        if (mailClient == null) {
-            mailClient = new MailClient(gouvPropertiesResolver.getMailUrl(), gouvPropertiesResolver.getMailJwt());
-        }
-        return mailClient;
-    }
-    
-    public FileClient getFileClient() {
-        if (fileClient == null) {
-            fileClient = new FileClient(gouvPropertiesResolver.getFileUrl(), gouvPropertiesResolver.getFileJwt());
-        }
-        return fileClient;
-    }
+		return null;
+	}
 
-    /**
-     * Retourne une version "cachée" des informations de la démarche
-     * 
-     * @return
-     */
-    public DemarcheDTO getDemarcheInfos() {
-        if (demarche == null) {
-            demarche = demarchesService.getDemarche(gouvPropertiesResolver.getDemarcheId());
-        }
-        return demarche;
-    }
-    
-    /**
-     * Retourne le nom complet de la démarche
-     * 
-     * @return
-     */
-    public String getDemarcheNom() {
-        return getDemarcheInfos().getNom();
-    }
+	public static String getAuthenticatedAgentName() {
+		Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (o instanceof User) {
+			return ((User) o).getNom();
+		}
 
-    /**
-     * Permet de récupérer une donnée d'une demande
-     * 
-     */
-    public static String getDemandeData(DemandeDTO demande, String key) {
-        if (demande.getData() != null) {
-            for (DemandeDataDTO demandeData : demande.getData()) {
-                if (demandeData.getKey().equals(key)) {
-                    return demandeData.getValue();
-                }
-            }
-        }
-        return null;
-    }
+		return null;
+	}
 
-    public static String getYear() {
-        return String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-    }
+	/**
+	 * Retourne le nom d'un usager à partir de son ID
+	 * 
+	 * @param usagerId
+	 * @return
+	 */
+	public String getUsagerNameFromID(Integer usagerId) {
+		UsagerBean u = usagersCache.get(usagerId);
+		if (u == null) {
+			return null;
+		}
+		String prenomNom = StringUtils.EMPTY;
+		if (u.getPrenom() != null) {
+			prenomNom += u.getPrenom();
+		}
+		if (u.getNom() != null) {
+			prenomNom += " " + u.getNom();
+		}
+		return prenomNom;
+	}
 
-    public static String getEnvName() {
-        return envName;
-    }
+	/**
+	 * Retourne le nom d'un utilisateur à partir de son matricule
+	 * 
+	 * @param userId
+	 * @return
+	 * @throws RestException
+	 */
+	public String getUserNameFromID(String matricule) throws RestException {
+		LOGGER.debug("getUserNameFromID() : Appel à Logon...");
+		User user = utilisateursCache.get(matricule);
+		if (user != null) {
+			return user.getNom();
+		}
+		return null;
+	}
 
-    public static String getEnvColor() {
-        return envColor;
-    }
-    
-    /**
-     * Permet de générer un DemandeFlatDTO à partir d'un DemandeDTO
-     * 
-     * @param demande
-     * @return
-     */
-    public DemandeFlatDTO getDemandeFlatDTO(DemandeDTO demande) {
-        DemandeFlatDTO flat = new DemandeFlatDTO();
-        flat.setAgentAffecteId(demande.getAgentAffecteId());
-        if (!StringUtils.isBlank(demande.getAgentAffecteId())) {
-            try {
-                flat.setAgentAffecteNom(getUserNameFromID(demande.getAgentAffecteId()));
-            } catch (RestException e) {
-                LOGGER.error("Erreur lors de la récupération du nom de l'agent affecté à la demande", e);
-            }
-        }
-        flat.setCanal(demande.getCanal().libelle);
-        flat.setCourrierDateReception(demande.getCourrierDateReception());
-        flat.setCourrierRefInterne(demande.getCourrierRefInterne());
-        flat.setDateCreation(demande.getDateCreation());
-        flat.setDernierStatut(demarchesDataProvider.getStatusLibelle(demande.getDernierStatut().getLibelle()));
-        flat.setIdentifiant(demande.getIdentifiant());
-        flat.setLangue(demande.getLangue());
-        flat.setObservations(demande.getObservations());
-        flat.setPkDemandes(demande.getPkDemandes());
-        flat.setUsagerId(demande.getUsagerId());
-        flat.setUsagerNom(getUsagerNameFromID(demande.getUsagerId()));
-        return flat;
-    }
-    
-    public static String getTitreStr(Short titre) {
-        if (titre == 0) {
-            return "Monsieur";
-        }
-        else if (titre == 1) {
-            return "Madame";
-        }
-        else if (titre == 2) {
-            return "Mademoiselle";
-        }
-        else {
-            return "";
-        }
-    }
+	/**
+	 * Génère un UUID version 1 (time+location based UUID) TODO copié de
+	 * afservlet, supprimer dans l'un des deux
+	 * 
+	 * @return
+	 */
+	public static UUID generateUUID() {
+		EthernetAddress addr = EthernetAddress.fromInterface();
+		TimeBasedGenerator uuidGenerator = Generators.timeBasedGenerator(addr);
+		UUID uuid = uuidGenerator.generate();
+		return uuid;
+	}
+
+	public MailClient getMailClient() {
+		if (mailClient == null) {
+			mailClient = new MailClient(gouvPropertiesResolver.getMailUrl(), gouvPropertiesResolver.getMailJwt());
+		}
+		return mailClient;
+	}
+
+	public FileClient getFileClient() {
+		if (fileClient == null) {
+			fileClient = new FileClient(gouvPropertiesResolver.getFileUrl(), gouvPropertiesResolver.getFileJwt());
+		}
+		return fileClient;
+	}
+
+	/**
+	 * Retourne une version "cachée" des informations de la démarche
+	 * 
+	 * @return
+	 */
+	public DemarcheDTO getDemarcheInfos() {
+		if (demarche == null) {
+			demarche = demarchesService.getDemarche(gouvPropertiesResolver.getDemarcheId());
+		}
+		return demarche;
+	}
+
+	/**
+	 * Retourne le nom complet de la démarche
+	 * 
+	 * @return
+	 */
+	public String getDemarcheNom() {
+		return getDemarcheInfos().getNom();
+	}
+
+	/**
+	 * Permet de récupérer une donnée d'une demande
+	 * 
+	 */
+	public static String getDemandeData(DemandeDTO demande, String key) {
+		if (demande.getData() != null) {
+			for (DemandeDataDTO demandeData : demande.getData()) {
+				if (demandeData.getKey().equals(key)) {
+					return demandeData.getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	public static String getYear() {
+		return String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+	}
+
+	public static String getEnvName() {
+		return envName;
+	}
+
+	public static String getEnvColor() {
+		return envColor;
+	}
+
+	/**
+	 * Permet de générer un DemandeFlatDTO à partir d'un DemandeDTO
+	 * 
+	 * @param demande
+	 * @return
+	 */
+	public DemandeFlatDTO getDemandeFlatDTO(DemandeDTO demande) {
+		DemandeFlatDTO flat = new DemandeFlatDTO();
+		flat.setAgentAffecteId(demande.getAgentAffecteId());
+		if (!StringUtils.isBlank(demande.getAgentAffecteId())) {
+			try {
+				flat.setAgentAffecteNom(getUserNameFromID(demande.getAgentAffecteId()));
+			} catch (RestException e) {
+				LOGGER.error("Erreur lors de la récupération du nom de l'agent affecté à la demande", e);
+			}
+		}
+		flat.setCanal(demande.getCanal().libelle);
+		flat.setCourrierDateReception(demande.getCourrierDateReception());
+		flat.setCourrierRefInterne(demande.getCourrierRefInterne());
+		flat.setDateCreation(demande.getDateCreation());
+		flat.setDernierStatut(demarchesDataProvider.getStatusLibelle(demande.getDernierStatut().getLibelle()));
+		flat.setIdentifiant(demande.getIdentifiant());
+		flat.setLangue(demande.getLangue());
+		flat.setObservations(demande.getObservations());
+		flat.setPkDemandes(demande.getPkDemandes());
+		flat.setUsagerId(demande.getUsagerId());
+		flat.setUsagerNom(getUsagerNameFromID(demande.getUsagerId()));
+		return flat;
+	}
+
+	public static String getTitreStr(Short titre) {
+		if (titre == 0) {
+			return "Monsieur";
+		} else if (titre == 1) {
+			return "Madame";
+		} else if (titre == 2) {
+			return "Mademoiselle";
+		} else {
+			return "";
+		}
+	}
 
 }

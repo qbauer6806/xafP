861cee74d165371f52282940a560d01e4d17a25c
Quentin DEME <qdeme@gouv.mc>
2018-12-07 07:19:47 +0000

    refs #11040 - Refactoring du DynamicJS
    git-svn-id: http://linuxic/jsrc/af/trunk@47937 f0974e12-e15a-48e7-82bd-60f10596da88

---
+++ af-back/src/main/java/mc/gouv/af/back/service/DynamicJSService.java
@@ -0,0 +1,7 @@
+package mc.gouv.af.back.service;
+
+public interface DynamicJSService {
+    
+    public String getResponse();
+
+}
---
+++ af-backweb/src/main/java/mc/gouv/af/backweb/ws/DynamicJSController.java
@@ -0,0 +1,27 @@
+package mc.gouv.af.backweb.ws;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mc.gouv.af.back.service.DynamicJSService;
+import mc.gouv.xboot.config.web.annotation.GouvRestController;
+
+@GouvRestController
+@RequestMapping("/dynamicjs")
+public class DynamicJSController {
+    
+    @Autowired
+    private DynamicJSService dynamicJSService;
+
+    @RequestMapping(value = "/dynamicjs.js", method = RequestMethod.GET, produces = "application/javascript")
+    public ResponseEntity<String> getResponse() {
+        
+        String js = dynamicJSService.getResponse();
+        
+        return new ResponseEntity<String>(js, null, HttpStatus.OK);
+    }
+    
+}

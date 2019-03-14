bceb9a77c2f97d39dafa8a5d23c462c2e7d2524d
David SAIDI PARTO <dsaidiparto.ext@gouv.mc>
2019-03-14 11:22:15 +0000

    Wiser est supprimer pour passer en Jenkins
    git-svn-id: http://linuxic/jsrc/af/trunk@48983 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-back/src/test/java/mc/gouv/af/back/mail/MailServiceTest.java
+++ af-back/src/test/java/mc/gouv/af/back/mail/MailServiceTest.java
@@ -1,13 +1,9 @@
 package mc.gouv.af.back.mail;
 
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.subethamail.wiser.Wiser;
 
 import mc.gouv.af.back.AfBackServiceTestConfiguration;
 
@@ -15,24 +11,5 @@ import mc.gouv.af.back.AfBackServiceTestConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=AfBackServiceTestConfiguration.class)
 public class MailServiceTest {
-	private Wiser wiser; 
-	
-	 @Value("${smtp.server.port}")
-	    private final Integer port = null;
-	 
-	@Autowired
-	private MailService mailService;
-	
-	@Before
-	public void setup() {
-		wiser = new Wiser();
-		wiser.setPort(port);
-		wiser.start();
-	}
-	
-//	@After
-//	punlic void tearDown( throws s)
-	
-	
 	
 }

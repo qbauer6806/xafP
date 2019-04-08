167ca3dfd3c921b994c03b562ba15d54c6103449
David SAIDI PARTO <dsaidiparto.ext@gouv.mc>
2019-04-08 15:31:10 +0000

    refs # 11967 : Le bug concernant de affichage incoherant le statut de la demande dupliquée est résolu
    git-svn-id: http://linuxic/jsrc/af/branches/v4.1.0@49184 f0974e12-e15a-48e7-82bd-60f10596da88

---
+++ af-back/src/main/java/mc/gouv/af/back/service/impl/IndexedEsDemandesStatutsServiceImpl.java
@@ -0,0 +1,59 @@
+package mc.gouv.af.back.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import mc.gouv.af.back.config.es.IndexationEnabledCondition;
+import mc.gouv.af.back.service.IndexedDemandeService;
+import mc.gouv.dem.service.impl.DemandesStatutsServiceImpl;
+import mc.gouv.dem.shared.model.DemandeDTO;
+
+/**
+ * @author dsaidiparto.ext
+ * 
+ *         Surcharge de DemandesStatutsServiceImpl pour indexer le statut de la demande sur Elastic Search
+ */
+@Service
+@Primary
+@Conditional(IndexationEnabledCondition.class)
+@Transactional(rollbackFor = Exception.class)
+public class IndexedEsDemandesStatutsServiceImpl extends DemandesStatutsServiceImpl {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexedEsDemandesStatutsServiceImpl.class);
+
+    @Autowired
+    private IndexedDemandeService indexedDemandeService;
+
+    /**
+     * {@inheritDoc}
+     * 
+     */
+    @Override
+    public DemandeDTO updateStatut(String demarcheId, Integer demandeId, String statut, String agentId,
+            Integer usagerId,
+            String codeMotif, String commentaire) {
+
+        DemandeDTO demandeDTO = null;
+        try {
+            demandeDTO = super.updateStatut(demarcheId, demandeId, statut, agentId, usagerId, codeMotif,
+                    commentaire);
+
+            indexDemandeStatus(demarcheId, demandeDTO.getPkDemandes());
+        } catch (Exception ex) {
+            LOGGER.error("l'erreur suivant  est survenu suit à  le mis à jour le statut \n" + ex.getMessage());
+        }
+
+        return demandeDTO;
+    }
+
+    private void indexDemandeStatus(String demarcheId, Integer pkDemande) throws Exception {
+        LOGGER.info("Indexation de Statuts de la demande");
+        DemandeDTO demandeDTO = indexedDemandeService.getDemande(demarcheId, pkDemande);
+        indexedDemandeService.sendToTopic(demandeDTO);
+    }
+}

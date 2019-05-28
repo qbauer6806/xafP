8dbc384eca359bdfcb5b0c8459a6f13acad7c725
Amine SOUABNI <asouabni.ext@gouv.mc>
2019-05-28 13:05:28 +0000

    refs #13389 - Création du role configuration pour les écrans d'administration de la recherche avancée
    git-svn-id: http://linuxic/jsrc/af/branches/v4.1.0@49661 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-backweb/src/main/java/mc/gouv/af/backweb/ws/RechercheAdminController.java
+++ af-backweb/src/main/java/mc/gouv/af/backweb/ws/RechercheAdminController.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,7 @@ import mc.gouv.af.backweb.controller.AbstractController;
 import mc.gouv.xboot.config.web.annotation.GouvRestController;
 
 @GouvRestController
+@Secured("ROLE_CONFIGURATION")
 @RequestMapping("/ws/admin/")
 @Conditional(IndexationEnabledCondition.class)
 public class RechercheAdminController extends AbstractController {

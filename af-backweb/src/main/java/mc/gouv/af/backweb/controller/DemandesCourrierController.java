13e02da024645f6fff4b02a7050bb3bb9828ac20
Manuel PAVONE <mpavone.ext@gouv.mc>
2019-07-17 15:45:26 +0000

    refs #14081 - encoding de l'url si des accents sont introduits dans le formulaire
    git-svn-id: http://linuxic/jsrc/af/branches/v4.1.0@50299 f0974e12-e15a-48e7-82bd-60f10596da88

--- af-backweb/src/main/java/mc/gouv/af/backweb/controller/DemandesCourrierController.java
+++ af-backweb/src/main/java/mc/gouv/af/backweb/controller/DemandesCourrierController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -102,13 +103,19 @@ public class DemandesCourrierController extends AbstractController {
 		if (gouvPropertiesResolver.getNovalidate()) {
 		    novalidate = "&novalidate=true";
 		}
-		
-		String redirect = "redirect:" + gouvPropertiesResolver.getFrontUrl() + "acces_teleservice.html?id=" + id
-                + "&international=fr" + novalidate + "&canal=" + demandesCourrierFormBean.getCanal() + "&langue="
-                + demandesCourrierFormBean.getLangue() + "&courrierDateReception=" + dateReceptionIso
-                + "&courrierRefInterne=" + demandesCourrierFormBean.getRefInterne() + "&target=/"
-                + gouvPropertiesResolver.getFrontFormStartPage() + "&creeParAgentId="
-                + AfBackUtils.getAuthenticatedAgentId() + "&sig=" + sig;
+
+		URIBuilder ub = new URIBuilder(gouvPropertiesResolver.getFrontUrl() + "acces_teleservice.html");
+		ub.addParameter("id", id);
+		ub.addParameter("international", "fr"+novalidate);
+		ub.addParameter("canal", demandesCourrierFormBean.getCanal());
+		ub.addParameter("langue", demandesCourrierFormBean.getLangue());
+		ub.addParameter("courrierDateReception", dateReceptionIso);
+		ub.addParameter("courrierRefInterne", demandesCourrierFormBean.getRefInterne());
+		ub.addParameter("target", "/" + gouvPropertiesResolver.getFrontFormStartPage());
+		ub.addParameter("creeParAgentId", AfBackUtils.getAuthenticatedAgentId());
+		ub.addParameter("sig", sig);
+
+		String redirect = "redirect:" + ub;
 		
 		LOGGER.info("URL de redirection vers le front : " + redirect);
 

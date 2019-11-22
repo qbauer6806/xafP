57fc7cea21501e909b038d67b93b6189ce5e0ea8
Mathieu <mboutelier.ext@gouv.mc>
2019-11-22 10:05:36 +0100

    refs #16114 - Bloquer le champ Référence interne
    bloquer le champ de référence interne pour la création de nouvelle demande courrier.

--- xaf-backweb/src/main/java/mc/gouv/xaf/backweb/formbean/DemandesCourrierFormBean.java
+++ xaf-backweb/src/main/java/mc/gouv/xaf/backweb/formbean/DemandesCourrierFormBean.java
@@ -1,5 +1,7 @@
 package mc.gouv.xaf.backweb.formbean;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 /**
@@ -15,7 +17,8 @@ public class DemandesCourrierFormBean {
 	@NotBlank
 	private String dateReception;
 
-	private String refInterne;
+    @Size(min = 0, max = 128, message = "La référence interne ne peut contenir plus de 128 caractères")
+    private String refInterne;
 
 	@NotBlank
 	private String canal;

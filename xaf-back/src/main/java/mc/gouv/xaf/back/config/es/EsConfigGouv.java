d02d154183b4a5ba539e4c350ec42f76a0cf6db9
Alimou DIALLO <adiallo.ext@gouv.mc>
2020-03-31 12:50:51 +0200

    refs #18723 : Ajout de l'algorithme de selection du master

--- xaf-back/src/main/java/mc/gouv/xaf/back/config/es/EsConfigGouv.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/config/es/EsConfigGouv.java
@@ -1,12 +1,20 @@
 package mc.gouv.xaf.back.config.es;
 
-import mc.gouv.xaf.back.properties.GouvPropertiesResolver;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.Node;
+import org.elasticsearch.client.NodeSelector;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -19,61 +27,104 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import mc.gouv.xaf.back.properties.GouvPropertiesResolver;
 
 @Configuration
 @Conditional(IndexationEnabledCondition.class)
 @EnableElasticsearchRepositories(basePackages = "mc.gouv.xaf.back.data.es.dao")
 public class EsConfigGouv {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EsConfigGouv.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(EsConfigGouv.class);
 
-    @Inject
-    private GouvPropertiesResolver gouvPropertiesResolver;
+	@Inject
+	private GouvPropertiesResolver gouvPropertiesResolver;
 
-    private final String GOUV_PROPERTIES_CHAR_SPLITTER = ",";
+	private final String GOUV_PROPERTIES_CHAR_SPLITTER = ",";
 
-    @Bean
-    public RestHighLevelClient client() {
-        String clusterHostsProperty = gouvPropertiesResolver.getEsClusterHosts();
-        String[] clusterHosts = new String[0];
-        if (StringUtils.isNotBlank(clusterHostsProperty)) {
-            clusterHosts = gouvPropertiesResolver.getEsClusterHosts().split(GOUV_PROPERTIES_CHAR_SPLITTER);
-        }
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	public RestHighLevelClient client() {
 
-        List<HttpHost> hosts = new ArrayList<>();
-        for (String clusterHost : clusterHosts) {
-            hosts.add(new HttpHost(clusterHost, gouvPropertiesResolver.getEsPort(), "http"));
-        }
+		String[] clusterHosts = StringUtils.split(gouvPropertiesResolver.getEsClusterHosts(),
+				GOUV_PROPERTIES_CHAR_SPLITTER);
 
-        Integer connectTimeout = gouvPropertiesResolver.getEsConnectTimeout();
-        Integer socketTimeout = gouvPropertiesResolver.getEsSocketTimeout();
-        RestClientBuilder builder = RestClient.builder(hosts.toArray(new HttpHost[0]))
-                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectTimeout(connectTimeout)
-                        .setSocketTimeout(socketTimeout));
+		List<HttpHost> hosts = new ArrayList<>();
+		for (String clusterHost : clusterHosts) {
+			hosts.add(new HttpHost(clusterHost, gouvPropertiesResolver.getEsPort()));
+		}
 
-        // Authentification elastisearch
-        String user = gouvPropertiesResolver.getEsUser();
-        String password = gouvPropertiesResolver.getEsPassword();
+		Integer connectTimeout = gouvPropertiesResolver.getEsConnectTimeout();
+		Integer socketTimeout = gouvPropertiesResolver.getEsSocketTimeout();
+		RestClientBuilder builder = RestClient.builder(hosts.toArray(new HttpHost[hosts.size()]))
+				.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectTimeout(connectTimeout)
+						.setSocketTimeout(socketTimeout));
+		
+		// Node selector
+		
+		builder.setNodeSelector(new NodeSelector() { 
+		    @Override
+		    public void select(Iterable<Node> nodes) {
+		        /*
+		         * Prefer any node that belongs to rack_one. If none is around
+		         * we will go to another rack till it's time to try and revive
+		         * some of the nodes that belong to rack_one.
+		         */
+		        boolean foundOne = false;
+		        for (Node node : nodes) {
+		            String rackId = node.getAttributes().get("rack_id").get(0);
+		            if ("rack_one".equals(rackId)) {
+		                foundOne = true;
+		                break;
+		            }
+		        }
+		        if (foundOne) {
+		            Iterator<Node> nodesIt = nodes.iterator();
+		            while (nodesIt.hasNext()) {
+		                Node node = nodesIt.next();
+		                String rackId = node.getAttributes().get("rack_id").get(0);
+		                if ("rack_one".equals(rackId) == false) {
+		                    nodesIt.remove();
+		                }
+		            }
+		        }
+		    }
+		});
 
-        if (StringUtils.isNotBlank(user) && StringUtils.isNotBlank(password)) {
-            LOGGER.info("Configuration d'elasticsearch avec Basic Auth");
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(user, password));
-            builder.setHttpClientConfigCallback(
-                    httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
-        }
+		// Authentification elastisearch
+		String user = gouvPropertiesResolver.getEsUser();
+		String password = gouvPropertiesResolver.getEsPassword();
 
-        RestHighLevelClient client = new RestHighLevelClient(builder);
-        return client;
-    }
+		if (StringUtils.isNotBlank(user) && StringUtils.isNotBlank(password)) {
+			LOGGER.info("Configuration d'elasticsearch avec Basic Auth");
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(user, password));
+			builder.setHttpClientConfigCallback(
+					httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+		}
 
-    @Bean
-    @Primary
-    public ElasticsearchRestTemplate elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
-    }
+		try (RestHighLevelClient client = new RestHighLevelClient(builder)) {
+
+
+			
+			
+			return client;
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	@Primary
+	public ElasticsearchRestTemplate elasticsearchTemplate() {
+		return new ElasticsearchRestTemplate(client());
+	}
 
 }

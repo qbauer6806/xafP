83e9e05784d3c3b89a7dbf106ea14ff6640ca964
Mathieu <mboutelier.ext@gouv.mc>
2020-05-19 10:37:43 +0200

    refs #19823 - Ajout d'un delegate pour générer l'histo en automatique

---
+++ xaf-back/src/main/java/mc/gouv/xaf/back/bpm/activiti/delegate/GouvBPMSauvegarderHistoriqueDelegate.java
@@ -0,0 +1,83 @@
+package mc.gouv.xaf.back.bpm.activiti.delegate;
+
+import mc.gouv.xaf.back.bpm.GouvBPMProcessVariableTypeEnum;
+import mc.gouv.xaf.back.properties.GouvPropertiesResolver;
+import mc.gouv.xaf.back.service.AfHistoService;
+import mc.gouv.xaf.back.service.data.DemandesHistoriqueService;
+import mc.gouv.xaf.back.service.utils.AfBackUtils;
+import mc.gouv.xaf.shared.dto.DemandeHistoriqueDTO;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.impl.el.Expression;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Classe appelée par activiti afin d'ajouter l'hisorique de changement de statut
+ *
+ * @author mboutelier.ext
+ */
+@Component
+public class GouvBPMSauvegarderHistoriqueDelegate implements JavaDelegate {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GouvBPMSauvegarderHistoriqueDelegate.class);
+
+    private Expression targetState;
+
+    @Autowired
+    private GouvPropertiesResolver gouvPropertiesResolver;
+
+    @Autowired
+    private AfHistoService histoService;
+
+    @Autowired
+    private DemandesHistoriqueService demandesHistoriqueService;
+
+    @Override
+    public void execute(DelegateExecution execution) {
+
+        LOGGER.info("==== xaf-back SAUVEGARDE HISTORIQUE ...");
+
+        String statut = getTargetState(execution);
+        Integer pkDemande = Integer.parseInt(execution.getProcessBusinessKey());
+
+        LOGGER.info("targetState = {}, pkDemande = {} ...", statut, pkDemande);
+
+        // Ajout d'une ligne à l'historique
+        DemandeHistoriqueDTO histo = histoService.traiterFinal(pkDemande, statut, AfBackUtils.getAuthenticatedAgentId());
+        LOGGER.info("Appel à DEM pour historique...");
+        try {
+            demandesHistoriqueService.saveHistoriqueActionAuto(gouvPropertiesResolver.getDemarcheId(), pkDemande, histo);
+        } catch (Exception e) {
+            LOGGER.error("Erreur lors de la création de l'historique {}", histo, e);
+        }
+
+        LOGGER.info("==== xaf-back SAUVEGARDE HISTORIQUE <FIN>");
+    }
+
+    public Expression getTargetState() {
+        return targetState;
+    }
+
+    public void setTargetState(Expression targetState) {
+        this.targetState = targetState;
+    }
+
+    /**
+     * Permet de retourner l'état dans lequel il faut mettre la demande Soit il a été défini dans le .bpmn (
+     * <serviceTask><extensionElements><activiti:field name="targetState"><activiti:string>AFFECTEE ...) Soit il a été
+     * défini dans la variable "targetState" du process auparavant
+     */
+    private String getTargetState(DelegateExecution execution) {
+        if (targetState != null) {
+            // L'état cible a été indiqué dans le .bpmn
+            return (String) targetState.getValue(execution);
+        } else {
+            // L'état cible a été indiqué dans une variable du process
+            return (String) execution.getVariableLocal(GouvBPMProcessVariableTypeEnum.MC_TARGETSTATE.name());
+        }
+    }
+
+}
--- xaf-back/src/main/java/mc/gouv/xaf/back/service/data/impl/DemandesHistoriqueServiceImpl.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/data/impl/DemandesHistoriqueServiceImpl.java
@@ -1,16 +1,5 @@
 package mc.gouv.xaf.back.service.data.impl;
 
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
 import mc.gouv.xaf.back.data.dao.DemandesHistoriqueRepository;
 import mc.gouv.xaf.back.data.entity.DemandeBO;
 import mc.gouv.xaf.back.data.entity.DemandesHistoriqueBO;
@@ -19,58 +8,69 @@ import mc.gouv.xaf.back.exception.DemarchesServiceException;
 import mc.gouv.xaf.back.service.data.DemandesHistoriqueService;
 import mc.gouv.xaf.back.service.data.DemandesService;
 import mc.gouv.xaf.shared.dto.DemandeHistoriqueDTO;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 /**
- * 
  * Service permettant la manipulation de l'historique des demandes.
- * 
- * @author qdeme
  *
+ * @author qdeme
  */
 @Component
 @Transactional(rollbackFor = Exception.class)
 public class DemandesHistoriqueServiceImpl implements DemandesHistoriqueService {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DemandesHistoriqueServiceImpl.class);
 
     @Autowired
     private DemandesHistoriqueRepository demandesHistoriqueRepository;
-    
+
     @Autowired
     private DemandesService demandesService;
-    
+
     @Override
     public List<DemandeHistoriqueDTO> getHistorique(String demarcheId, Integer demandeId) {
-        
+
         // Jette une exception si la demande n'existe pas
         demandesService.getCheckDemarcheDemandeDTO(demarcheId, demandeId, false);
-        
+
         List<DemandesHistoriqueBO> demandeHistorique = demandesHistoriqueRepository.findByFkDemandesPkDemandes(demandeId);
-        
+
         LOGGER.info("Transformation bo -> dto ...");
         return DemandesHistoriqueTransformer.bo2Dto(demandeHistorique);
     }
 
     @Override
     public DemandeHistoriqueDTO saveHistorique(String demarcheId, Integer demandeId, DemandeHistoriqueDTO demandeHistoriqueDto) {
-        
         if ((StringUtils.isBlank(demandeHistoriqueDto.getAgentId()) && demandeHistoriqueDto.getUsagerId() == null) ||
                 (!StringUtils.isBlank(demandeHistoriqueDto.getAgentId()) && demandeHistoriqueDto.getUsagerId() != null)) {
             throw new DemarchesServiceException("Soit agentId ou usagerId doit être renseigné", HttpStatus.BAD_REQUEST);
         }
+        return saveHistoriqueActionAuto(demarcheId, demandeId, demandeHistoriqueDto);
+    }
 
+    @Override
+    public DemandeHistoriqueDTO saveHistoriqueActionAuto(String demarcheId, Integer demandeId, DemandeHistoriqueDTO demandeHistoriqueDto) {
         DemandeBO demandeBo = demandesService.getCheckDemarcheDemandeBO(demarcheId, demandeId, false);
 
         LOGGER.info("Transformation dto -> bo");
         DemandesHistoriqueBO demandeHistoriqueBo = DemandesHistoriqueTransformer.dto2Bo(demandeHistoriqueDto);
-        
+
         demandeHistoriqueBo.setFkDemandes(demandeBo);
         demandeHistoriqueBo.setFkStatut(demandeBo.getDernierStatut());
         demandeHistoriqueBo.setDate(new Date());
-        
+
         LOGGER.info("Sauvegarde en base...");
         demandeHistoriqueBo = demandesHistoriqueRepository.save(demandeHistoriqueBo);
-        
+
         LOGGER.info("Transformation bo -> dto ...");
         return DemandesHistoriqueTransformer.bo2Dto(demandeHistoriqueBo);
     }

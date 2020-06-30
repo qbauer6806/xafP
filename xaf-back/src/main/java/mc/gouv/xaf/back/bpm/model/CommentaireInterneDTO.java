d2c34ba31dbc56329b17a6f5ee776ad4213dc0b4
Mathieu <mboutelier.ext@gouv.mc>
2020-06-30 10:58:04 +0200

    refs refs #20762 - Bugfix: commentaires internes pour Activiti

--- xaf-back/src/main/java/mc/gouv/xaf/back/bpm/model/CommentaireInterneDTO.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/bpm/model/CommentaireInterneDTO.java
@@ -1,5 +1,7 @@
 package mc.gouv.xaf.back.bpm.model;
 
+import java.util.Date;
+
 /**
  * Classe représentant un commentaire interne entre intervenants du back-office
  *
@@ -9,5 +11,33 @@ public class CommentaireInterneDTO extends mc.gouv.af.back.bpm.model.Commentaire
 
     private static final long serialVersionUID = -3630330943792014082L;
 
+    private String agentId;
 
+    private Date date;
+
+    private String commentaire;
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(String agentId) {
+        this.agentId = agentId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
 }

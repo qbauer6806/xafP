44550f4bbb42d41c58ea13585f282ad95a1e1aba
Mathieu <mboutelier.ext@gouv.mc>
2020-07-02 14:52:12 +0200

    refs #19336 - Ajout d'un message pour les erreurs d'historique

--- xaf-shared/src/main/java/mc/gouv/xaf/shared/SharedMessages.java
+++ xaf-shared/src/main/java/mc/gouv/xaf/shared/SharedMessages.java
@@ -5,7 +5,9 @@ public class SharedMessages {
     public static final String SUCCESS_MESSAGES = "successMessages";
     public static final String TRANSFORMATION_BO_DTO = "Transformation bo -> dto ...";
     public static final String TRANSFORMATION_DTO_BO = "Transformation dto -> bo ...";
+
     public static final String APPEL_SAVE_HISTORIQUE = "Appel au service de sauvegarde de l'historique...";
+    public static final String ERREUR_HISTORIQUE = "Erreur lors de la création de l'historique {}";
 
     private SharedMessages() {
     }

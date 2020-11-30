5679b52848458f824eb53fbbf564a79368449175
Manuel Pavone <mpavone.ext@gouv.mc>
2020-11-30 16:42:15 +0100

    refs #24069 - [INTERFACE] Maj RC20

--- xaf-shared/src/main/java/mc/gouv/xaf/shared/itg/resid/dto/ResidUtilisateurDTO.java
+++ xaf-shared/src/main/java/mc/gouv/xaf/shared/itg/resid/dto/ResidUtilisateurDTO.java
@@ -11,11 +11,21 @@ public class ResidUtilisateurDTO implements Serializable {
 
     private String utilisateurNomPrenom;
 
+    private String utilisateurMatricule;
+
     public String getUtilisateurNomPrenom() {
         return utilisateurNomPrenom;
     }
 
     public void setUtilisateurNomPrenom(String utilisateurNomPrenom) {
         this.utilisateurNomPrenom = utilisateurNomPrenom;
+    }
+
+    public String getUtilisateurMatricule() {
+        return utilisateurMatricule;
+    }
+
+    public void setUtilisateurMatricule(String utilisateurMatricule) {
+        this.utilisateurMatricule = utilisateurMatricule;
     }
 }

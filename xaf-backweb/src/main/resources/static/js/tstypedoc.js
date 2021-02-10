6a0ea95d743a16d76f7bd7fb96a13a33ccfd8f75
Manuel Pavone <mpavone.ext@gouv.mc>
2021-02-10 15:03:25 +0100

    refs #26477 - Modification du bouton "Enregistrer type fichier" Onglet fichier - Evaluation heuristique

--- xaf-backweb/src/main/resources/static/js/tstypedoc.js
+++ xaf-backweb/src/main/resources/static/js/tstypedoc.js
@@ -4,24 +4,31 @@ var complementsJSON = {}
 updateTypedocJSON = (event) => {
     const idEvent = event.id;
     const isComp = idEvent.includes("comp");
-    $("#typedoc-button").attr("disabled", false);
     if (isComp) {
+        $("#typedoc-complements-button").attr("disabled", false);
         const pkDemandesFiles = idEvent.replace("comp", "");
         complementsJSON[pkDemandesFiles] = event.value;
     } else {
+        $("#typedoc-files-button").attr("disabled", false);
         const pkDemandesFiles = idEvent.replace("file", "");
         filesJSON[pkDemandesFiles] = event.value;
     }
-    console.log(complementsJSON);
-    console.log(filesJSON);
 }
 
-$("#typedoc-button").click(function (e) {
-    console.log(complementsJSON);
-    console.log(filesJSON);
-    $("#typedoc-complements").prop('value', JSON.stringify(complementsJSON));
-    $("#typedoc-files").prop('value', JSON.stringify(filesJSON));
-    $("#confirmer-typedoc-button").click();
+$("#typedoc-files-button").click(function (e) {
+    execEnregistrerFichiers();
     e.preventDefault();
     return false;
 });
+
+$("#typedoc-complements-button").click(function (e) {
+    execEnregistrerFichiers();
+    e.preventDefault();
+    return false;
+});
+
+var execEnregistrerFichiers = function() {
+    $("#typedoc-complements").prop('value', JSON.stringify(complementsJSON));
+    $("#typedoc-files").prop('value', JSON.stringify(filesJSON));
+    $("#confirmer-typedoc-button").click();
+};

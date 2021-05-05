342e661271975a0f3f973b9f15e7b3d7a09c5b7c
Xavier DECOOL <XDECOOL.EXT@monaco-gouvernement.mc>
2021-05-05 11:20:51 +0200

    refs #28725 - Gestion des warnings RESID

--- xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/dto/ResidWarningDTO.java
+++ xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/dto/ResidWarningDTO.java
@@ -2,6 +2,11 @@ package mc.gouv.xaf.back.dsp.dto;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResidWarningDTO implements Serializable {
 
 	private static final long serialVersionUID = 1016532880273751925L;

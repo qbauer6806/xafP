ea6a12d549b0443a6e5e7a6f0d04984a9333f5e5
Decool <xdecool.ext@gouv.mc>
2021-10-15 16:50:53 +0200

    refs #33289 - [BO] Traitement de la demande - Erreur 500 suite à tentative de génération du récap pour une demande ayant & dans le nom de l'entreprise partenaire

--- xaf-back/src/main/java/mc/gouv/xaf/back/service/pdf/recap/impl/PdfRecapGenerationServiceImpl.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/pdf/recap/impl/PdfRecapGenerationServiceImpl.java
@@ -139,7 +139,8 @@ public class PdfRecapGenerationServiceImpl implements PdfRecapGenerationService 
                     : "Aucune demande d'informations complémentaires.";
 
             LOGGER.info("Génération du code HTML de la récap...");
-            String htmlRecap = demandeRecapHTMLService.getHTMLDemandeContenuRecap(demande, true);
+            // refs #33280 - [BO] Traitement de la demande - Erreur 500 suite à tentative de génération du récap pour une demande ayant ''&" dans le nom de l'entreprise partenaire
+            String htmlRecap = demandeRecapHTMLService.getHTMLDemandeContenuRecap(demande, true).replaceAll("&[^(#38)(amp)(AMP)]", "&#38; ");
 
             LOGGER.info("Création d'un fichier temporaire pour stocker le HTML...");
             htmlSource = File.createTempFile("tmpRecapHtml", ".html");

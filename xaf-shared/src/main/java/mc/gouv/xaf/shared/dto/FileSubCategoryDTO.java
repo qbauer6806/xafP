8c5362f9e506961fc58d02276a95a3254e62f3f4
Quentin DEME <qdeme@gouv.mc>
2021-11-29 14:50:01 +0100

    refs #34613 - Permettre l'affichage de sous-catégories (ou sous-sections) au sein des catégories de l'onglet Fichiers de la page de traitement d'une demande

--- xaf-shared/src/main/java/mc/gouv/xaf/shared/dto/FileSubCategoryDTO.java
+++ xaf-shared/src/main/java/mc/gouv/xaf/shared/dto/FileSubCategoryDTO.java
@@ -2,13 +2,11 @@ package mc.gouv.xaf.shared.dto;
 
 import java.util.List;
 
-public class FileCategoryDTO {
+public class FileSubCategoryDTO {
 
 	private String name;
 
 	private List<DemandeFileDTO> files;
-	
-	private List<FileSubCategoryDTO> subCategories;
 
 	private boolean typedoc;
 
@@ -34,13 +32,5 @@ public class FileCategoryDTO {
 
 	public void setTypedoc(boolean typedoc) {
 		this.typedoc = typedoc;
-	}
-
-	public List<FileSubCategoryDTO> getSubCategories() {
-		return subCategories;
-	}
-
-	public void setSubCategories(List<FileSubCategoryDTO> subCategories) {
-		this.subCategories = subCategories;
 	}
 }

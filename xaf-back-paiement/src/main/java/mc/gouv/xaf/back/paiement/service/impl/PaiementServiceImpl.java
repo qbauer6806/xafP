ba6188a068a324bc32bd44425307f48814bd7918
Edouard GERMAIN <egermain.ext@gouv.mc>
2022-07-05 00:00:43 +0200

    refs #41337 integration brique paiement modif -> to squash

--- xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/PaiementServiceImpl.java
+++ xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/PaiementServiceImpl.java
@@ -31,6 +31,7 @@ import mc.gouv.xaf.shared.stc.dto.PaiementDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
@@ -76,6 +77,7 @@ public class PaiementServiceImpl implements PaiementService {
     private ReferenceFactoryService referenceFactoryService;
 
     @Autowired
+    @Lazy
     private UsagersCache usagersCache;
     @Autowired
     private PaiementClient paiementClient;

1a2881aadb522588ee1485c38dd6e16a6e1b56fd
Edouard GERMAIN <egermain.ext@gouv.mc>
2022-07-11 09:43:30 +0200

    refs #41337 integration brique paiement modif

--- xaf-shared/src/main/java/mc/gouv/xaf/shared/stc/config/MoneticoPaiementConfig.java
+++ xaf-shared/src/main/java/mc/gouv/xaf/shared/stc/config/MoneticoPaiementConfig.java
@@ -3,7 +3,7 @@ package mc.gouv.xaf.shared.stc.config;
 public class MoneticoPaiementConfig {
     public static final String MONETICOPAIEMENT_EPTNUMBER = "7527409";
     public static final String MONETICOPAIEMENT_VERSION = "3.0";
-    public static final String MONETICOPAIEMENT_COMPANYCODE = "PERMC";
+    public static final String MONETICOPAIEMENT_COMPANYCODE = "PERMCIFRAME";
 
     public final static String MONETICOPAIEMENT_URLOK = "http://localhost:3000/demande_paiement_ok.html";
     public final static String MONETICOPAIEMENT_URLKO = "http://localhost:3000/demande_paiement_erreur.html";

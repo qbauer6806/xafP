d4501bb8850d1a96798d43b3633642033c52c8d8
Manuel PAVONE <mpavone.ext@gouv.mc>
2022-07-26 18:52:44 +0200

    refs #42378 - Création d'un Service pour contacter l'API RIO pour la GED

--- xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/bpm/activiti/delegate/GouvBPMArchivageDelegate.java
+++ xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/bpm/activiti/delegate/GouvBPMArchivageDelegate.java
@@ -5,6 +5,7 @@ import mc.gouv.xaf.back.paiement.service.ArchivageService;
 import mc.gouv.xaf.back.properties.GouvPropertiesResolver;
 import mc.gouv.xaf.back.service.data.DemandesService;
 import mc.gouv.xaf.shared.dto.DemandeDTO;
+import mc.gouv.xaf.shared.dto.DemandeFileDTO;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
@@ -12,7 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class GouvBPMArchivageDelegate implements JavaDelegate {
@@ -20,6 +23,7 @@ public class GouvBPMArchivageDelegate implements JavaDelegate {
     private static final Logger LOGGER = LoggerFactory.getLogger(GouvBPMArchivageDelegate.class);
 
     public static final String MC_REFERENCE_PERMIS = "MC_REFERENCE_PERMIS";
+    public static final String MC_ORDRE_FICHIERS = "MC_ORDRE_FICHIERS";
 
     @Autowired
     private GouvBPM gouvBPM;
@@ -40,11 +44,29 @@ public class GouvBPMArchivageDelegate implements JavaDelegate {
         Integer demandeId = Integer.parseInt(execution.getProcessBusinessKey());
 
         String reference = (String) gouvBPM.getProcessBusinessVariables(demandeId).get(MC_REFERENCE_PERMIS);
+        String ordreFichiers = (String) gouvBPM.getProcessBusinessVariables(demandeId).get(MC_ORDRE_FICHIERS);
 
         DemandeDTO demandeDto = demandesService.getDemande(gouvPropertiesResolver.getDemarcheId(),
                 demandeId);
 
-        archivageService.archivageDocuments(reference, Arrays.asList(demandeDto.getFichiers()));
+        List<DemandeFileDTO> fichiers = Arrays.asList(demandeDto.getFichiers());
+
+        // Gestion de l'ordre d'envoi
+        // Si une variable d'ordre est définie, trier les fichiers
+        if (!ordreFichiers.isEmpty()) {
+            List<DemandeFileDTO> fichiersTries = new ArrayList<>();
+            for (String typeDoc : ordreFichiers.split(",")) {
+                for (DemandeFileDTO file : fichiers) {
+                    if (typeDoc.equals(file.getTypedoc())) {
+                        fichiersTries.add(file);
+                    }
+                }
+            }
+            fichiers = fichiersTries;
+        }
+
+
+        archivageService.archivageDocuments(reference, fichiers);
 
         LOGGER.info("==== xaf-back-stc Archivage <fin>");
     }

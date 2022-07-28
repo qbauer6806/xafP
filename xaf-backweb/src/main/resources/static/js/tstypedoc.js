4d1c565ee5a529b3e9be9202de2161c5aec0f6b7
Decool <xdecool.ext@gouv.mc>
2022-07-28 11:03:33 +0200

    refs #42590 - Refactorisation du panel de typage des documents pour le rendre plus générique

--- xaf-backweb/src/main/resources/static/js/tstypedoc.js
+++ xaf-backweb/src/main/resources/static/js/tstypedoc.js
@@ -1,3 +1,5 @@
+/*<![CDATA[*/
+
 var filesJSON = {};
 var complementsJSON = {}
 
@@ -35,3 +37,150 @@ var execEnregistrerFichiers = function() {
     $("#typedoc-files").prop('value', JSON.stringify(filesJSON));
     $("#confirmer-typedoc-button").click();
 };
+
+var setCurrentPopup = function(previewButton) {
+			const href = previewButton.parentNode.getAttribute('value');
+			document.getElementById(previewButton.id).setAttribute('disabled', 'disabled');
+			document.getElementById(previewButton.id).classList.add("loading");
+			// On close les tooltip
+			$("[class='tooltip fade bottom in']").remove();
+			if(!href.toLowerCase().endsWith('.pdf')) {
+				$.magnificPopup.open({
+					items: {
+						src: href
+					},
+					type:'image',
+					image: {
+	   					 tError: 'Erreur lors de l\'affichage de l\'image'
+	  				}
+				});
+				$.magnificPopup.instance.close = function () {
+					enableButton(previewButton.id);
+					$.magnificPopup.proto.close.call(this);
+				}
+			} else {
+				PDFJS.getDocument(href).then(
+					function(pdf) {
+			            // Supprimer tous les anciens canvas générés dans le dom
+			            $('.pdf-canvas').remove();
+			            //Start with first page
+			            pdf.getPage(1).then(
+			            	createPdfCanvas.bind(null, 'canvas-container-file-pdf', 1, pdf.numPages, pdf))
+			                	.then(openPdfModal('#preview-container-file-pdf', previewButton.id), function () {
+			                    	alert("Erreur lors de l'affichage du pdf");
+			                });
+		        		},
+	        		function(error){
+	        			enableButton(previewButton.id);
+	        			$.notify({
+							message: "Erreur lors de l'affichage du pdf"
+						},{
+							type: 'danger'
+						});
+	        		}	
+	        	);
+			}
+		};
+		
+		$("#allDownloadButton").click(function() {
+			var zipName = demandeId+"_Fichiers";
+			var url = domZip+demandeId+"?fileType=all&zipName="+zipName;
+			location.href = url;
+		});
+		
+		$("#validesDownloadButton").click(function() {
+			if(clickable) {
+				var zipName = demandeId+"_Fichiers_Valides";
+				var url = domZip+demandeId+"?fileType=valides&zipName="+zipName;
+				location.href = url;
+			}
+		});
+		
+		$("#allDownloadButtonPdf").click(function() {
+			var pdfName = demandeId+"_Fichiers";
+			var url = domPdf+demandeId+"?fileType=all&pdfName="+pdfName;
+			$.ajax({
+				url: url,
+				method: 'GET',
+				xhrFields: {
+		            responseType: 'blob'
+		        },
+				beforeSend: function (xhr) {
+					setPanelLoading();
+				},
+				success: function(data, textStatus, xhr) {
+					disablePanelLoading();
+					var a = document.createElement('a');
+		            var url = window.URL.createObjectURL(data);
+		            a.href = url;
+		            a.download = pdfName+'.pdf';
+		            document.body.append(a);
+		            a.click();
+		            a.remove();
+		            window.URL.revokeObjectURL(url);
+				}
+			});
+		});
+		
+		$("#validesDownloadButtonPdf").click(function() {
+			if(clickable) {
+				var pdfName = demandeId+"_Fichiers";
+				var url = domPdf+demandeId+"?fileType=valides&pdfName="+pdfName;
+				$.ajax({
+					url: url,
+					method: 'GET',
+					xhrFields: {
+			            responseType: 'blob'
+			        },
+					beforeSend: function (xhr) {
+						setPanelLoading();
+					},
+					success: function(data, textStatus, xhr) {
+						disablePanelLoading();
+						var a = document.createElement('a');
+			            var url = window.URL.createObjectURL(data);
+			            a.href = url;
+			            a.download = pdfName+'.pdf';
+			            document.body.append(a);
+			            a.click();
+			            a.remove();
+			            window.URL.revokeObjectURL(url);
+					}
+				});
+			}
+		});
+		
+		var setPanelLoading = function() {
+			var tab2 = document.getElementById('tab2');
+			var tabLoader = document.getElementById('tabLoader');
+			var loaderMessage = document.getElementById('loaderMessage');
+			tab2.classList.remove("active");
+			loaderMessage.classList.remove("hide");
+			tabLoader.classList.add("active");
+		}
+		
+		var disablePanelLoading = function() {
+			var tab2 = document.getElementById('tab2');
+			var tabLoader = document.getElementById('tabLoader');
+			var loaderMessage = document.getElementById('loaderMessage');
+			tabLoader.classList.remove("active");
+			tab2.classList.add("active");
+			loaderMessage.classList.add("hide");
+		}
+		
+		enableButton = (buttonId) => {
+			// Déverouillage des champs en cas d'annulation
+			document.getElementById(buttonId).removeAttribute('disabled');
+			document.getElementById(buttonId).classList.remove("loading");
+		};
+		
+		var openPdfModal = function(containerId, buttonId) {
+			$.fancybox.open({
+				src: containerId,
+				type: 'inline',
+				opts: {
+					afterClose: enableButton(buttonId)
+				}
+			});
+		};
+		/*]]>*/

47e07b06561fdc430dda63b59fdfc57728226e1b
Decool <xdecool.ext@gouv.mc>
2022-08-08 15:27:36 +0200

    refs #42803 - [PAIEMENT] Changement du nom de la facture généré au moment de la capture d'un paiement

--- xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/FactureServiceImpl.java
+++ xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/FactureServiceImpl.java
@@ -1,5 +1,17 @@
 package mc.gouv.xaf.back.paiement.service.impl;
 
+import static mc.gouv.xaf.back.paiement.LoggerMethodeUtils.logStartMethod;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import mc.gouv.xaf.back.paiement.client.FactureClient;
 import mc.gouv.xaf.back.paiement.enums.PaiementDemandeDataKeysEnum;
 import mc.gouv.xaf.back.paiement.service.FactureService;
@@ -9,19 +21,9 @@ import mc.gouv.xaf.back.service.data.DemandesDataService;
 import mc.gouv.xaf.back.service.data.DemandesFilesService;
 import mc.gouv.xaf.back.service.data.DemandesService;
 import mc.gouv.xaf.back.service.itg.file.FileService;
+import mc.gouv.xaf.back.service.utils.AfBackUtils;
 import mc.gouv.xaf.shared.dto.DemandeDTO;
 import mc.gouv.xaf.shared.dto.DemandeFileDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.util.Date;
-import java.util.Optional;
-
-import static mc.gouv.xaf.back.paiement.LoggerMethodeUtils.logStartMethod;
 
 @Service
 public class FactureServiceImpl implements FactureService {
@@ -50,7 +52,7 @@ public class FactureServiceImpl implements FactureService {
         Optional<InputStream> optionalFactureIS = factureClient.getFacture(reference, demande);
         if (optionalFactureIS.isPresent()) {
             ByteArrayOutputStream output = new ByteArrayOutputStream();
-            String fileName = reference + ".pdf";
+            String fileName = "Justificatif_Facture_"+ demande.getIdentifiant() + "_" + AfBackUtils.generateFileDateSuffix() + ".pdf";
             String url = fileService.saveFile(demande, fileName, gouvPropertiesResolver.getContainerId(), "application/pdf", optionalFactureIS.get(), output);
             output.close();
             optionalFactureIS.get().close();

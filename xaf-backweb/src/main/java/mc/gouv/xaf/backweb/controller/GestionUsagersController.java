acb2e126422cf739d2f8d2913c727d1e5ae92dd4
Quentin DEME <qdeme@gouv.mc>
2022-08-11 10:22:03 +0200

    refs #42868 - Trier la liste des pays de la page de création d'un usager courrier par ordre alphabétique

--- xaf-backweb/src/main/java/mc/gouv/xaf/backweb/controller/GestionUsagersController.java
+++ xaf-backweb/src/main/java/mc/gouv/xaf/backweb/controller/GestionUsagersController.java
@@ -1,6 +1,7 @@
 package mc.gouv.xaf.backweb.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -36,6 +37,7 @@ import mc.gouv.xaf.back.service.data.UsagersCourrierService;
 import mc.gouv.xaf.back.service.itg.rest.PaysCache;
 import mc.gouv.xaf.back.service.itg.rest.UsagersCache;
 import mc.gouv.xaf.back.service.utils.AfBackUtils;
+import mc.gouv.xaf.back.service.utils.PaysComparator;
 import mc.gouv.xaf.back.service.utils.UsagersUtils;
 import mc.gouv.xaf.backweb.dto.UsagerCourrierResultDTO;
 import mc.gouv.xaf.backweb.formbean.TransfertDemandesFormBean;
@@ -485,6 +487,11 @@ public class GestionUsagersController extends AbstractController {
                 listePaysP4.add(p);
             }
         }
+        PaysComparator paysComparator = new PaysComparator();
+        Collections.sort(listePaysP1, paysComparator);
+        Collections.sort(listePaysP2, paysComparator);
+        Collections.sort(listePaysP3, paysComparator);
+        Collections.sort(listePaysP4, paysComparator);
         mav.addObject("listePaysP1", listePaysP1);
         mav.addObject("listePaysP2", listePaysP2);
         mav.addObject("listePaysP3", listePaysP3);

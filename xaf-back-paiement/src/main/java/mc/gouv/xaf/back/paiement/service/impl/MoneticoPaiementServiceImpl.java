085439b2b13a87824bfc2f160a09c412a08a178c
Mathieu <mboutelier.ext@gouv.mc>
2022-09-20 15:43:03 +0200

    refs #43165 - [Paiement - BO] Rollback multi-thread

--- xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/MoneticoPaiementServiceImpl.java
+++ xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/service/impl/MoneticoPaiementServiceImpl.java
@@ -22,11 +22,9 @@ import mc.gouv.xaf.back.paiement.service.itg.MoneticoPaiementService;
 import mc.gouv.xaf.back.paiement.service.itg.PaiementSecurityService;
 import mc.gouv.xaf.back.properties.GouvPropertiesResolver;
 import mc.gouv.xaf.back.service.data.DemandesDataService;
-import mc.gouv.xaf.back.service.data.PropertiesService;
 import mc.gouv.xaf.back.service.itg.rest.UsagersCache;
 import mc.gouv.xaf.shared.dto.DemandeDataDTO;
 import mc.gouv.xaf.shared.dto.GichuniUsagerDTO;
-import mc.gouv.xaf.shared.dto.PropertiesDTO;
 import mc.gouv.xaf.shared.dto.itg.monetico.MoneticoResponseDTO;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -49,8 +47,8 @@ import static mc.gouv.xaf.back.service.utils.AfBackUtils.DTF_AAAA_MM_JJ;
 
 @Service
 public class MoneticoPaiementServiceImpl implements MoneticoPaiementService {
+    public static final String UPDATE_PAIEMENT_DATA_THREAD = "THREAD_UPDATE_PAIEMENT_DATA_REF_";
     private static final Logger LOGGER = LoggerFactory.getLogger(MoneticoPaiementServiceImpl.class);
-    public static final String UPDATE_PAIEMENT_DATA_THREAD = "UPDATE_PAIEMENT_DATA_";
     private static final String CODE_RETOUR_OK = "0";
     private static final String CODE_RETOUR_KO = "1";
 
@@ -74,9 +72,6 @@ public class MoneticoPaiementServiceImpl implements MoneticoPaiementService {
 
     @Autowired
     private PaiementSecurityService paiementSecurityService;
-
-    @Autowired
-    private PropertiesService propertiesService;
 
     @Autowired
     private ReferenceFactoryService referenceFactoryService;
@@ -226,11 +221,7 @@ public class MoneticoPaiementServiceImpl implements MoneticoPaiementService {
         if (status.equals("payetest") || status.equals("paiement")) {
             moyenPaiementBO.setMoyenPaiementStatut(MoyenPaiementStatutEnum.VALIDE);
             List<CommandeDemandeBO> commandeDemandeBOList = commandeDemandeRepository.findByCommande_PkCommande(moyenPaiementBO.getCommande().getPkCommande());
-            Timestamp date = Timestamp.valueOf(LocalDateTime.now());
-            for (CommandeDemandeBO commandeDemandeBO : commandeDemandeBOList) {
-                DemandeBO demandeBO = commandeDemandeBO.getDemande();
-                updateDemandeData(demandeBO, dateValidite, moneticoResponseDTO, date);
-            }
+            updateDemandeData(commandeDemandeBOList, dateValidite, moneticoResponseDTO);
         } else {
             moyenPaiementBO.setMoyenPaiementStatut(MoyenPaiementStatutEnum.INVALIDE);
         }
@@ -285,48 +276,53 @@ public class MoneticoPaiementServiceImpl implements MoneticoPaiementService {
 
     // TODO sauvegarder le statut du paiement de manière plus correct que dans les demandes data
     @Async
-    void updateDemandeData(DemandeBO demandeBO, LocalDateTime dateValidite, MoneticoResponseDTO moneticoResponseDTO, Timestamp datePaiement) {
+    void updateDemandeData(List<CommandeDemandeBO> commandeDemandeBOList, LocalDateTime dateValidite, MoneticoResponseDTO moneticoResponseDTO) {
         Thread t = new Thread(() -> {
-            Integer pkDemande = demandeBO.getPkDemandes();
-            Integer usagerId = demandeBO.getFkAccess().getUsagerId();
-            GouvBPMUser user = new GouvBPMUser();
-            user.setId(usagerId.toString());
+            Timestamp date = Timestamp.valueOf(LocalDateTime.now());
+            for (CommandeDemandeBO commandeDemandeBO : commandeDemandeBOList) {
+                DemandeBO demandeBO = commandeDemandeBO.getDemande();
 
-            LOGGER.info("========== Mise à jour des données de la demande {}...", pkDemande);
-            String demarcheId = gouvPropertiesResolver.getDemarcheId();
-            Map<String, String> datas = new HashMap<>();
-            datas.put(PaiementDemandeDataKeysEnum.DATE_PAIEMENT.name(), LocalDateTime.now().format(DTF_AAAA_MM_JJ));
-            datas.put(PaiementDemandeDataKeysEnum.DATE_EXPIRATION_EMPREINTE.name(), dateValidite.format(DTF_AAAA_MM_JJ));
-            datas.put(PaiementDemandeDataKeysEnum.STATUT_PAIEMENT.name(), PaiementStatutEnum.EMPREINTE_VALIDE.name());
-            datas.put(PaiementDemandeDataKeysEnum.MOYEN_PAIEMENT.name(), moneticoResponseDTO.getModepaiement());
-            datas.put(PaiementDemandeDataKeysEnum.MOYEN_PAIEMENT_REFERENCE.name(), moneticoResponseDTO.getReference());
-            demandesDataService.saveOrUpdateDemandeDatas(demarcheId, pkDemande, datas);
+                Integer pkDemande = demandeBO.getPkDemandes();
+                Integer usagerId = demandeBO.getFkAccess().getUsagerId();
+                GouvBPMUser user = new GouvBPMUser();
+                user.setId(usagerId.toString());
 
-            LOGGER.info("Ajout de l'historique de paiement...");
-            PaiementHistoriqueBO historique = new PaiementHistoriqueBO();
-            historique.setFkDemande(demandeBO);
-            historique.setContenu("Usager " + demandeBO.getUsagerPrenom() + " " + demandeBO.getUsagerNom() + " : Effectue une empreinte bancaire");
-            historique.setStatut(PaiementStatutEnum.EMPREINTE_VALIDE.name());
-            historique.setDate(datePaiement);
-            historique.setUsagerId(demandeBO.getFkAccess().getUsagerId());
-            paiementHistoriqueRepository.save(historique);
+                LOGGER.info("========== Mise à jour des données de la demande {}...", pkDemande);
+                String demarcheId = gouvPropertiesResolver.getDemarcheId();
+                Map<String, String> datas = new HashMap<>();
+                datas.put(PaiementDemandeDataKeysEnum.DATE_PAIEMENT.name(), LocalDateTime.now().format(DTF_AAAA_MM_JJ));
+                datas.put(PaiementDemandeDataKeysEnum.DATE_EXPIRATION_EMPREINTE.name(), dateValidite.format(DTF_AAAA_MM_JJ));
+                datas.put(PaiementDemandeDataKeysEnum.STATUT_PAIEMENT.name(), PaiementStatutEnum.EMPREINTE_VALIDE.name());
+                datas.put(PaiementDemandeDataKeysEnum.MOYEN_PAIEMENT.name(), moneticoResponseDTO.getModepaiement());
+                datas.put(PaiementDemandeDataKeysEnum.MOYEN_PAIEMENT_REFERENCE.name(), moneticoResponseDTO.getReference());
+                demandesDataService.saveOrUpdateDemandeDatas(demarcheId, pkDemande, datas);
 
-            LOGGER.info("Progression dans le BPM...");
-            Map<String, Object> variables = gouvBPM.getProcessBusinessVariables(pkDemande);
-            variables.put(GouvBPMProcessVariableTypeEnum.MC_TARGETSTATE_ORIGINATOR_USAGER.name(), usagerId.toString());
-            variables.put(GouvBPMProcessVariableTypeEnum.MC_TARGETSTATE_ORIGINATOR_AGENT.name(), null);
-            gouvBPM.setProcessBusinessVariables(pkDemande, variables);
+                LOGGER.info("Ajout de l'historique de paiement...");
+                PaiementHistoriqueBO historique = new PaiementHistoriqueBO();
+                historique.setFkDemande(demandeBO);
+                historique.setContenu("Usager " + demandeBO.getUsagerPrenom() + " " + demandeBO.getUsagerNom() + " : Effectue une empreinte bancaire");
+                historique.setStatut(PaiementStatutEnum.EMPREINTE_VALIDE.name());
+                historique.setDate(date);
+                historique.setUsagerId(demandeBO.getFkAccess().getUsagerId());
+                paiementHistoriqueRepository.save(historique);
 
-            GouvBPMTask task = gouvBPM.getActiveTasksForDemande(pkDemande).get(0);
-            try {
-                gouvBPM.claimTask(task, user);
-                gouvBPM.completeTask(task, pkDemande);
-            } catch (Exception e1) {
-                LOGGER.error("Erreur lors du claim et de la complétion de la tache du paiement");
-                throw new DemarchesServiceException(e1.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                LOGGER.info("Progression dans le BPM...");
+                Map<String, Object> variables = gouvBPM.getProcessBusinessVariables(pkDemande);
+                variables.put(GouvBPMProcessVariableTypeEnum.MC_TARGETSTATE_ORIGINATOR_USAGER.name(), usagerId.toString());
+                variables.put(GouvBPMProcessVariableTypeEnum.MC_TARGETSTATE_ORIGINATOR_AGENT.name(), null);
+                gouvBPM.setProcessBusinessVariables(pkDemande, variables);
+
+                GouvBPMTask task = gouvBPM.getActiveTasksForDemande(pkDemande).get(0);
+                try {
+                    gouvBPM.claimTask(task, user);
+                    gouvBPM.completeTask(task, pkDemande);
+                } catch (Exception e1) {
+                    LOGGER.error("Erreur lors du claim et de la complétion de la tache du paiement");
+                    throw new DemarchesServiceException(e1.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
         });
-        t.setName(UPDATE_PAIEMENT_DATA_THREAD + demandeBO.getIdentifiant());
+        t.setName(UPDATE_PAIEMENT_DATA_THREAD + moneticoResponseDTO.getReference());
         t.start();
     }
 }

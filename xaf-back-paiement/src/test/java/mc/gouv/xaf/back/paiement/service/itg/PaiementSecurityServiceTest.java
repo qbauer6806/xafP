bf69a36a7d4f47c854f7651c0bcd1c8511b41a6d
Mathieu <mboutelier.ext@gouv.mc>
2022-10-03 17:39:32 +0200

    Revert "refs #43080 - HMAC"
    This reverts commit 7bed60bef7d8b48a07ee99ec4d3bcd18cc122ade.

--- xaf-back-paiement/src/test/java/mc/gouv/xaf/back/paiement/service/itg/PaiementSecurityServiceTest.java
+++ xaf-back-paiement/src/test/java/mc/gouv/xaf/back/paiement/service/itg/PaiementSecurityServiceTest.java
@@ -3,6 +3,7 @@ package mc.gouv.xaf.back.paiement.service.itg;
 import mc.gouv.xaf.back.paiement.dto.ContexteCommandeDTO;
 import mc.gouv.xaf.back.paiement.dto.PaiementDTO;
 import mc.gouv.xaf.shared.dto.itg.monetico.MoneticoResponseDTO;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,30 +33,6 @@ public class PaiementSecurityServiceTest {
 
     @Test
     public void getHmacStringInterfaceRetourTest() {
-//        MoneticoResponseDTO responseDTOTest = new MoneticoResponseDTO();
-//        responseDTOTest.setTpe("7527409");
-//        responseDTOTest.setAuthentification("ewogICAiZGV0YWlscyIgOiB7CiAgICAgICJBUmVzIiA6ICJDIiwKICAgICAgIkNSZXMiIDogIlkiLAogICAgICAibGlhYmlsaXR5U2hpZnQiIDogIlkiLAogICAgICAibWVyY2hhbnRQcmVmZXJlbmNlIiA6ICJjaGFsbGVuZ2VfbWFuZGF0ZWQiLAogICAgICAidHJhbnNhY3Rpb25JRCIgOiAiMzQ1MTM2YzgtMmY5OC00ZjIxLWIxMDctOTlmNTZlMzM4YTU0IgogICB9LAogICAicHJvdG9jb2wiIDogIjNEU2VjdXJlIiwKICAgInN0YXR1cyIgOiAiYXV0aGVudGljYXRlZCIsCiAgICJ2ZXJzaW9uIiA6ICIyLjEuMCIKfQo=");
-//        responseDTOTest.setBincb("00000100");
-//        responseDTOTest.setBrand("VI");
-//        responseDTOTest.setCbmasquee("00000100******02");
-//        responseDTOTest.setCodeRetour("payetest");
-//        responseDTOTest.setCvx("oui");
-//        responseDTOTest.setDate("21/07/2022_a_15:56:26");
-//        responseDTOTest.setEcard("non");
-//        responseDTOTest.setHpancb("10181E9A22519D864FA98E154C3E2FFBC6F69C30");
-//        responseDTOTest.setIpclient("82.113.11.254");
-//        responseDTOTest.setModepaiement("CB");
-//        responseDTOTest.setMontant("80.0EUR");
-//        responseDTOTest.setOriginecb("FRA");
-//        responseDTOTest.setOriginetr("MCO");
-//        responseDTOTest.setReference("YnZEx97Puhvq");
-//        responseDTOTest.setTypecompte("inconnu");
-//        responseDTOTest.setUsage("inconnu");
-//        responseDTOTest.setVld("1222");
-//        responseDTOTest.setTexteLibre("Demander l echange d un permis de conduire etranger - Commande du 03/10/2022:16:23:48 - demandes [PEC-20221003-I0J9]");
-//        String macTest = paiementSecurityService.getHmacStringInterfaceRetour(responseDTOTest);
-//        System.out.println(macTest);
-
         MoneticoResponseDTO responseDTO = new MoneticoResponseDTO();
         responseDTO.setTpe("7527409");
         responseDTO.setAuthentification("ewogICAicHJvdG9jb2wiIDogIjNEU2VjdXJlIiwKICAgInN0YXR1cyIgOiAibm90X2Vucm9sbGVkIiwKICAgInZlcnNpb24iIDogIjEuMC4yIgp9Cg==");

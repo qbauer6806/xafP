acae333986e818353b9759e9a030048f9318839d
agaidi.ext <agaidi.ext@312-DA119-19.monaco-gouvernement.mc>
2023-06-21 17:42:42 +0200

    refs #50119 - [DEV] Intégration code API Ulis

--- xaf-servlet/src/main/java/mc/gouv/xaf/servlet/InitialDemandeServlet.java
+++ xaf-servlet/src/main/java/mc/gouv/xaf/servlet/InitialDemandeServlet.java
@@ -3,6 +3,7 @@ package mc.gouv.xaf.servlet;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,15 +16,18 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import mc.gouv.xaf.servlet.dto.UsagerInfosDTO;
 import mc.gouv.xaf.servlet.util.AppFactoryServletUtils;
+import mc.gouv.xaf.shared.SessionConstant;
 import mc.gouv.xaf.shared.SharedMessages;
 import mc.gouv.xaf.shared.dto.DonneesExternesDTO;
 import mc.gouv.xaf.shared.dto.DonneesExternesDemandeDTO;
+import mc.gouv.xaf.shared.dto.PropertiesDTO;
 
 /**
  * Servlet mettant à disposition les donnees externes
@@ -36,6 +40,9 @@ public class InitialDemandeServlet extends AbstractAfServlet {
     private static final String MCONNECT_PARAM_GIVENNAME = "GivenName";
     private static final String MCONNECT_PARAM_FAMILYNAME = "FamilyName";
     private static final String MCONNECT_PARAM_BIRTHDATE = "BirthDatetime";
+    private static final String ULIS_PARAM_CONTRAT = "numerocontrat";
+    private static final String ULIS_PARAM_FACTURE = "numerocontrat";
+    private static final String ULIS_PARAM_TIERS = "numerotiers";
     private static final Logger LOGGER = LoggerFactory.getLogger(InitialDemandeServlet.class);
 
     /**
@@ -77,52 +84,64 @@ public class InitialDemandeServlet extends AbstractAfServlet {
             return;
         }
 
-        if (usagerInfosDTO.ismConnect()) {
+        ObjectMapper omapper = new ObjectMapper();
+        omapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-            DonneesExternesDTO donneesMConnectDTO;
+        List<PropertiesDTO> properties = getAfApiClient().getFrontProperties();
+        Map<String, String[]> data = new HashMap<>();
+        /* liste des params propres aux TS, que le front peut utiliser pour faire une recherche de donnees externes */
 
-            Map<String, String[]> data = new HashMap<>();
+        PropertiesDTO property = properties.stream()
+                .filter(prop -> "XAF_DONNEES_EXTERNES_PARAMETER_LIST".equals(prop.getKey())).findFirst().orElse(null);
+
+        if (property != null) {
+            try {
+                List<String> parameters = omapper.readValue(property.getValue(), new TypeReference<List<String>>() {
+                });
+                parameters.forEach(
+                        parameterName -> data.put(parameterName, request.getParameterMap().get(parameterName)));
+            } catch (IOException e) {
+                LOGGER.warn(
+                        "Impossible de traiter XAF_DONNEES_EXTERNES_PARAMETER_LIST. Vérifier le format de la properties",
+                        e);
+            }
+        }
+
+        DonneesExternesDTO donneesMConnectDTO;
+
+        try {
 
             if (usagerInfosDTO.ismConnect()) {
                 JsonNode usagerJson = usagerInfosDTO.getDonneesExternes();
-                ObjectMapper omapper = new ObjectMapper();
-                omapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                data.put("numerocontrat", request.getParameterMap().get("numerocontrat"));
-                data.put("numerotiers", request.getParameterMap().get("numerotiers"));
-                data.put("numerofacture", request.getParameterMap().get("numerofacture"));
+                donneesMConnectDTO = omapper.treeToValue(usagerJson, DonneesExternesDTO.class);
                 data.put("usagerId", new String[] { usagerInfosDTO.getId() + "" });
-                try {
-                    donneesMConnectDTO = omapper.treeToValue(usagerJson, DonneesExternesDTO.class);
-                    data.put(MCONNECT_PARAM_FAMILYNAME,
-                            new String[] { donneesMConnectDTO.getMconnect().getFamilyName() });
-                    data.put(MCONNECT_PARAM_GIVENNAME,
-                            new String[] { donneesMConnectDTO.getMconnect().getGivenName() });
-                    data.put(MCONNECT_PARAM_BIRTHDATE, new String[] { new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
-                            .format(donneesMConnectDTO.getMconnect().getBirthDatetime()) });
-
-                    JsonNode retour = getAfApiClient().getDonneesExternes(usagerInfosDTO.getId(), data);
-                    ObjectMapper mapper = new ObjectMapper();
-                    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-                    mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
-
-                    DonneesExternesDemandeDTO resultSearch = mapper.treeToValue(retour,
-                            DonneesExternesDemandeDTO.class);
-
-                    response.setContentType(MediaType.APPLICATION_JSON);
-                    if (resultSearch.getStatut() == 200)
-                        mapper.writeValue(response.getOutputStream(), resultSearch.getDemande());
-                    response.setStatus(resultSearch.getStatut());
-                    response.getOutputStream().flush();
-                } catch (JsonProcessingException e) {
-                    response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-                    LOGGER.error("JsonProcessingException. Impossible de recuperer les donnees externes", e);
-                } catch (IOException e) {
-                    response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-                    LOGGER.error("IOException. Impossible de recuperer les donnees externes", e);
-                }
-
+                data.put(MCONNECT_PARAM_FAMILYNAME, new String[] { donneesMConnectDTO.getMconnect().getFamilyName() });
+                data.put(MCONNECT_PARAM_GIVENNAME, new String[] { donneesMConnectDTO.getMconnect().getGivenName() });
+                data.put(MCONNECT_PARAM_BIRTHDATE, new String[] { new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+                        .format(donneesMConnectDTO.getMconnect().getBirthDatetime()) });
             }
 
+            JsonNode retour = getAfApiClient().getDonneesExternes(usagerInfosDTO.getId(), data);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+
+            DonneesExternesDemandeDTO resultSearch = mapper.treeToValue(retour, DonneesExternesDemandeDTO.class);
+
+            response.setContentType(MediaType.APPLICATION_JSON);
+            if (resultSearch.getStatut() == 200) {
+                mapper.writeValue(response.getOutputStream(), resultSearch.getDemande());
+                /* TODO: check si on stocke l'objet en session de façon à le récupérer à la creation de la demande */
+                request.getSession().setAttribute(SessionConstant.SESSION_INITIAL_DEMANDE, resultSearch.getDemande());
+            }
+            response.setStatus(resultSearch.getStatut());
+            response.getOutputStream().flush();
+        } catch (JsonProcessingException e) {
+            response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            LOGGER.error("JsonProcessingException. Impossible de recuperer les donnees externes", e);
+        } catch (IOException e) {
+            response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            LOGGER.error("IOException. Impossible de recuperer les donnees externes", e);
         }
 
         LOGGER.info("====================== Fin /InitialDemandeServlet doGet()");

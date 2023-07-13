cfb4a68c0ea31fdd4c45782e9591f70d1d8b8f9e
ttorreze.ext <ttorreze.ext@gouv.mc>
2023-07-13 17:34:06 +0200

    [WIP] Mini modifs TUs

--- xaf-servlet/src/test/java/mc/gouv/xaf/servlet/DocHolderServletsTest.java
+++ xaf-servlet/src/test/java/mc/gouv/xaf/servlet/DocHolderServletsTest.java
@@ -2,35 +2,28 @@ package mc.gouv.xaf.servlet;
 
 import mc.gouv.xaf.servlet.util.AppFactoryServletUtils;
 import mc.gouv.xaf.shared.SharedMessages;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DocHolderServletsTest {
-
-    @Mock
-    private HttpServletResponse response;
-
-    @Mock
-    private HttpServletRequest request;
-
-    @Mock
-    private RequestDispatcher requestDispatcher;
 
     @InjectMocks
     private DocHolderSearchServlet docHolderSearchServlet;
@@ -40,16 +33,19 @@ public class DocHolderServletsTest {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
-        /*StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);*/
-        //when(response.getWriter()).thenReturn(writer);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
 
-        MockedStatic<AppFactoryServletUtils> servletUtilsMocked = mockStatic(AppFactoryServletUtils.class);
-        servletUtilsMocked.when(() -> AppFactoryServletUtils.getLoggedUser(any())).thenReturn(null);
+        try(MockedStatic<AppFactoryServletUtils> servletUtilsMocked = mockStatic(AppFactoryServletUtils.class)) {
+            servletUtilsMocked.when(() -> AppFactoryServletUtils.getLoggedUser(any())).thenReturn(null);
 
-        docHolderSearchServlet.doGet(request, response);
+            docHolderSearchServlet.doGet(request, response);
+        }
 
         Logger logger = LoggerFactory.getLogger(DocHolderSearchServlet.class);
         verify(logger, times(1)).error(SharedMessages.UTILISATEUR_NON_AUTORISE);
+
+        assertEquals(response.getStatus(), HttpStatus.SC_UNAUTHORIZED);
     }
 }

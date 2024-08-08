b6287e41113b819d8ccad95f749c87d24850b06c
Ugo EL KOUBI <uelkoubi.ext@gouv.mc>
2024-08-08 22:30:45 +0200

    fix batch

--- src/main/java/mc/gouv/xaf/xaf12batch/reader/DemandeEsItemReader.java
+++ src/main/java/mc/gouv/xaf/xaf12batch/reader/DemandeEsItemReader.java
@@ -6,10 +6,13 @@ import javax.annotation.PostConstruct;
 import mc.gouv.xaf.xaf12batch.dto.DemandeEsDTO;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.batch.item.ItemReader;
@@ -24,6 +27,8 @@ public class DemandeEsItemReader implements ItemReader<DemandeEsDTO> {
     private SearchResponse searchResponse;
     private int currentIndex = 0;
 
+    private final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1));
+
     @Value("${application.name}")
     private String applicationName;
 
@@ -35,6 +40,7 @@ public class DemandeEsItemReader implements ItemReader<DemandeEsDTO> {
     @PostConstruct
     public void init() throws IOException {
         SearchRequest searchRequest = new SearchRequest(applicationName + "-index");
+        searchRequest.scroll(scroll);
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
         boolQuery.must(QueryBuilders.existsQuery("dernierStatut"));
@@ -51,7 +57,15 @@ public class DemandeEsItemReader implements ItemReader<DemandeEsDTO> {
             SearchHit searchHit = searchResponse.getHits().getHits()[currentIndex++];
             System.out.println(searchHit.getSourceAsString());
             return new ObjectMapper().readValue(searchHit.getSourceAsString(), DemandeEsDTO.class);
+        } else {
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(searchResponse.getScrollId());
+            scrollRequest.scroll(scroll);
+            searchResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+            currentIndex = 0;
+            if (searchResponse.getHits().getHits().length == 0) {
+                return null; // Fin de la pagination
+            }
+            return read();
         }
-        return null;
     }
 }

984f12ca2e5c7e143e5fea6b05cdaa44e73830cb
MONACO-GOUV\uelkoubi.ext <uelkoubi.ext@gouv.mc>
2024-09-25 12:09:33 +0200

    fix cache

--- xaf-front/src/main/java/mc/gouv/xaf/front/filter/NoCacheFilter.java
+++ xaf-front/src/main/java/mc/gouv/xaf/front/filter/NoCacheFilter.java
@@ -3,37 +3,24 @@ package mc.gouv.xaf.front.filter;
 import java.io.IOException;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class NoCacheFilter implements Filter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
-
-    @Override
-    public void doFilter(
-            ServletRequest servletRequest,
-            ServletResponse servletResponse,
-            FilterChain filterChain
-    )
-            throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        // pour certaines versions d'IE, no-cache veut dire no-store et donc si on
-        // l'utilise, on n'aura pas de cache des CSS/IMG non plus.
-        // Par contre, le Pragma HTTP 1.0 en no-cache ne pose pas de soucis et est
-        // équivalent à must-revalidate plus ou moins.
-        response.setHeader("Cache-Control", "must-revalidate, max-age=0");
-        response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires", 0L);
-        filterChain.doFilter(servletRequest, servletResponse);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ContentCachingResponseWrapper responseCacheWrapperObject = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+        filterChain.doFilter(servletRequest, responseCacheWrapperObject);
+        responseCacheWrapperObject.addHeader("Cache-Control", "no-cache, must-revalidate");
+        responseCacheWrapperObject.copyBodyToResponse();
     }
-
-    @Override
-    public void destroy() {}
 }

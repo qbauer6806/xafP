c9d2ccaff8ca5142d0e1c738af8d48b6df0a7e8d
Ugo EL KOUBI <uelkoubi.ext@gouv.mc>
2024-10-09 16:54:16 +0200

    #64141 - [CFG] Configurer la recherche avancée

--- xaf-back/src/main/java/mc/gouv/xaf/back/service/utils/RechercheCourriersUtils.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/utils/RechercheCourriersUtils.java
@@ -93,7 +93,11 @@ public class RechercheCourriersUtils extends RechercheUtils {
             // process du full text search
             List<Path> paths = new ArrayList<>();
             paths.add(root.get("searchVector"));
+            // permettre de chercher sur les caractéristiques de la demande (identifiant...)
             paths.add(root.join("fkDemandes", JoinType.LEFT).get("searchVector"));
+            // permettre de chercher sur les fichiers et compléments fichiers
+            paths.add(root.join("fkDemandes", JoinType.LEFT).join("files", JoinType.LEFT).get("searchVector"));
+            paths.add(root.join("fkDemandes", JoinType.LEFT).join("demandesComplements", JoinType.LEFT).join("files", JoinType.LEFT).get("searchVector"));
             setFTSPredicates(paths, predicates, cb, texte);
         }
 

ed89bf8786d66b5787eaac7313f55f3e662dd494
Mazid DIALLO <amdiallo.ext@gouv.mc>
2025-01-08 14:37:17 +0100

    refs [TECH] ajout d'une méthode avec comparator pour trier les sub catégories

--- xaf-back/src/main/java/mc/gouv/xaf/back/service/DemandeFilesCategorizer.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/DemandeFilesCategorizer.java
@@ -1,9 +1,11 @@
 package mc.gouv.xaf.back.service;
 
+import java.util.Comparator;
 import java.util.List;
 
 import mc.gouv.xaf.shared.dto.DemandeDTO;
 import mc.gouv.xaf.shared.dto.FileCategoryDTO;
+import mc.gouv.xaf.shared.dto.FileSubCategoryDTO;
 
 /**
  * Service permettant de lister les catégories de fichiers ainsi que les fichiers associés
@@ -13,5 +15,15 @@ import mc.gouv.xaf.shared.dto.FileCategoryDTO;
 public interface DemandeFilesCategorizer {
 
     List<FileCategoryDTO> getCategoriesAndFiles(DemandeDTO demande);
+
+    /**
+     * Permets de construire la liste des catégories et leurs fichiers en utilisant le comparateur donné en entrée
+     * pour trier les sub catégories
+     *
+     * @param demande    la demande qui les contient les fichiers
+     * @param comparator pour trier les sub catégories
+     * @return la liste des catégories construites
+     */
+    List<FileCategoryDTO> getCategoriesAndFiles(DemandeDTO demande, Comparator<FileSubCategoryDTO> comparator);
 
 }
--- xaf-back/src/main/java/mc/gouv/xaf/back/service/impl/DemandeFilesCategorizerImpl.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/impl/DemandeFilesCategorizerImpl.java
@@ -6,6 +6,8 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import jakarta.validation.constraints.NotNull;
 import mc.gouv.xaf.back.data.transformer.DemandesComplementsFilesTransformer;
 import mc.gouv.xaf.back.service.DemandeFilesCategorizer;
 import mc.gouv.xaf.back.service.data.PropertiesService;
@@ -36,6 +38,75 @@ public class DemandeFilesCategorizerImpl implements DemandeFilesCategorizer {
 
     @Autowired
     private PropertiesService propertiesService;
+
+    /**
+     * @param demande
+     * @return
+     */
+    @Override
+    public List<FileCategoryDTO> getCategoriesAndFiles(DemandeDTO demande) {
+        return getCategoriesAndFiles(demande, new FileSubCategoryComparator());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param demande
+     * @param comparator
+     * @return
+     */
+    @Override
+    public List<FileCategoryDTO> getCategoriesAndFiles(DemandeDTO demande, @NotNull Comparator<FileSubCategoryDTO> comparator) {
+        DemandeFileDTO[] demandeFiles = demande.getFichiers();
+        List<FileCategoryDTO> categories = new ArrayList<>();
+
+        FileCategoryDTO catDemandeInitiale = new FileCategoryDTO();
+        catDemandeInitiale.setName(FileUtils.CAT_INITIALE);
+        catDemandeInitiale.setTypedoc(true);
+        // Sous-catégories
+        PropertiesDTO prop = propertiesService.getProperty(XAF_SECTIONS_FICHIERS_DEMANDE);
+        String propValue = prop != null ? prop.getValue() : "";
+        Map<String, String> mapClesSections = AfBackUtils.getListFromDemProperty(propValue);
+        Map<String, FileSubCategoryDTO> listeSections = new HashMap<>();
+
+        if (demandeFiles != null) {
+            for (DemandeFileDTO file : demande.getFichiers()) {
+                if (file.getMeta() == null || file.getMeta().startsWith(FileUtils.META_FRONT)) {
+                    String idSection = AfBackUtils.getSectionFromMetaFichier(file.getMeta());
+                    listeSections.computeIfAbsent(idSection, key -> this.creerSubCategory(key, mapClesSections))
+                            .getFiles().add(file);
+                }
+            }
+        }
+
+        if (CollectionUtils.isNotEmpty(listeSections.values())) {
+            List<FileSubCategoryDTO> subCats = new ArrayList<>(listeSections.values());
+            subCats.sort(comparator);
+
+            catDemandeInitiale.setSubCategories(subCats);
+        }
+        catDemandeInitiale.setFiles(new ArrayList<>());
+        categories.add(catDemandeInitiale);
+
+        FileCategoryDTO catComplements = new FileCategoryDTO();
+        catComplements.setName(FileUtils.CAT_COMPLEMENTS);
+        catComplements.setTypedoc(true);
+        catComplements.setFiles(fichiersComplementaires(demande.getComplements()));
+        categories.add(catComplements);
+
+        FileCategoryDTO catFichiersRemisAdministration = new FileCategoryDTO();
+        catFichiersRemisAdministration.setName(FileUtils.CAT_ADMINISTRATION);
+        catFichiersRemisAdministration.setFiles(fichiersAdministration(demandeFiles));
+        categories.add(catFichiersRemisAdministration);
+
+        FileCategoryDTO catFichiersInternes = new FileCategoryDTO();
+        catFichiersInternes.setName(FileUtils.CAT_INTERNES);
+        catFichiersInternes.setFiles(fichiersInternes(demandeFiles));
+        catFichiersInternes.setTypedoc(false);
+        categories.add(catFichiersInternes);
+
+        return categories;
+    }
 
     private List<DemandeFileDTO> fichiersComplementaires(DemandeComplementsDTO[] complements) {
         List<DemandeFileDTO> files = new ArrayList<>();
@@ -74,59 +145,6 @@ public class DemandeFilesCategorizerImpl implements DemandeFilesCategorizer {
             }
         }
         return files;
-    }
-
-    @Override
-    public List<FileCategoryDTO> getCategoriesAndFiles(DemandeDTO demande) {
-        DemandeFileDTO[] demandeFiles = demande.getFichiers();
-        List<FileCategoryDTO> categories = new ArrayList<>();
-
-        FileCategoryDTO catDemandeInitiale = new FileCategoryDTO();
-        catDemandeInitiale.setName(FileUtils.CAT_INITIALE);
-        catDemandeInitiale.setTypedoc(true);
-        // Sous-catégories
-        PropertiesDTO prop = propertiesService.getProperty(XAF_SECTIONS_FICHIERS_DEMANDE);
-        String propValue = prop != null ? prop.getValue() : "";
-        Map<String, String> mapClesSections = AfBackUtils.getListFromDemProperty(propValue);
-        Map<String, FileSubCategoryDTO> listeSections = new HashMap<>();
-
-        if (demandeFiles != null) {
-            for (DemandeFileDTO file : demande.getFichiers()) {
-                if (file.getMeta() == null || file.getMeta().startsWith(FileUtils.META_FRONT)) {
-                    String idSection = AfBackUtils.getSectionFromMetaFichier(file.getMeta());
-                    listeSections.computeIfAbsent(idSection, key -> this.creerSubCategory(key, mapClesSections))
-                            .getFiles().add(file);
-                }
-            }
-        }
-
-        if (CollectionUtils.isNotEmpty(listeSections.values())) {
-            List<FileSubCategoryDTO> subCats = new ArrayList<>(listeSections.values());
-            subCats.sort(new FileSubCategoryComparator());
-
-            catDemandeInitiale.setSubCategories(subCats);
-        }
-        catDemandeInitiale.setFiles(new ArrayList<>());
-        categories.add(catDemandeInitiale);
-
-        FileCategoryDTO catComplements = new FileCategoryDTO();
-        catComplements.setName(FileUtils.CAT_COMPLEMENTS);
-        catComplements.setTypedoc(true);
-        catComplements.setFiles(fichiersComplementaires(demande.getComplements()));
-        categories.add(catComplements);
-
-        FileCategoryDTO catFichiersRemisAdministration = new FileCategoryDTO();
-        catFichiersRemisAdministration.setName(FileUtils.CAT_ADMINISTRATION);
-        catFichiersRemisAdministration.setFiles(fichiersAdministration(demandeFiles));
-        categories.add(catFichiersRemisAdministration);
-
-        FileCategoryDTO catFichiersInternes = new FileCategoryDTO();
-        catFichiersInternes.setName(FileUtils.CAT_INTERNES);
-        catFichiersInternes.setFiles(fichiersInternes(demandeFiles));
-        catFichiersInternes.setTypedoc(false);
-        categories.add(catFichiersInternes);
-
-        return categories;
     }
 
     public static class FileSubCategoryComparator implements Comparator<FileSubCategoryDTO> {

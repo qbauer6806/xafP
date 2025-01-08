0abc5c3016e663b02b6caa2e2b331a95e1459b95
Ugo EL KOUBI <uelkoubi.ext@gouv.mc>
2025-01-08 11:08:55 +0100

    rajouter la description dans resetMarqueurs

--- src/main/java/mc/gouv/xaf/xaf12batch/marqueurs/MarqueursService.java
+++ src/main/java/mc/gouv/xaf/xaf12batch/marqueurs/MarqueursService.java
@@ -1,8 +1,9 @@
 package mc.gouv.xaf.xaf12batch.marqueurs;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
 import mc.gouv.xaf.xaf12batch.demandesconfig.DemandesConfigService;
 import mc.gouv.xaf.xaf12batch.dto.DemandeConfigBO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +26,16 @@ public class MarqueursService {
         List<DemandeConfigBO> configs = demandesConfigService.getConfigsBO();
         for (DemandeConfigBO config : configs) {
             List<MarqueurDTO> marqueurDTOS = new ArrayList<>();
+
             setMarqueursFromModelPaths(
-                    demandesConfigService.getModelPaths(config.getContenu().get("modelPaths").get("rechercheAvancee")),
-                    marqueurDTOS, config.getBuildId());
+                    demandesConfigService.getModelPaths(config.getContenu().get("modelPaths").get("marqueurs")),
+                    marqueurDTOS, config.getBuildId(), config.getContenu().get("recap").get("sections"));
             marqueursRepository.saveAll(marqueursTransformer.dtos2Bos(marqueurDTOS));
         }
     }
 
-    private void setMarqueursFromModelPaths(List<String> modelPaths, List<MarqueurDTO> marqueurDTOS, String buildId) {
+    private void setMarqueursFromModelPaths(List<String> modelPaths, List<MarqueurDTO> marqueurDTOS, String buildId,
+            JsonNode sections) {
         for (String modelPath : modelPaths) {
             String id = pathToCamelCase(modelPath);
             // si le marqueur est déjà présent (du précédent buildId par exemple), on ne génère pas le marqueur
@@ -41,10 +44,89 @@ public class MarqueursService {
                 marqueur.setChemin(modelPath);
                 marqueur.setIdentifiant(id);
                 marqueur.setBuildId(buildId);
+                marqueur.setDescription(getDescriptionFromTranslations(sections, modelPath));
                 marqueurDTOS.add(marqueur);
             }
         }
     }
+
+    private String getDescriptionFromTranslations(JsonNode sections, String modelPath) {
+        String modifiedModelPath = modelPath;
+        String suffixeFound = null;
+        String[] possibleSuffixesToRemove = { "ligne1", "ligne2", "ligne3", "ville", "pays", "codePostal", "bic",
+                "iban", "titulaire", "indicatif", "numero" };
+        for (String suffix : possibleSuffixesToRemove) {
+            String suffixDot = "." + suffix;
+            if (modelPath.endsWith(suffixDot)) {
+                suffixeFound = suffix;
+                modifiedModelPath = modelPath.substring(0, modelPath.length() - suffixDot.length());
+                break;
+            }
+        }
+        // récupérer tous les champs, qu'ils soient dans des sections ou des sous sections
+        List<JsonNode> champsNodes = sections.findValues("champs");
+        for (JsonNode champs : champsNodes) {
+            for (JsonNode champ : champs) {
+                if (champ.get("path").asText().equals(modifiedModelPath)) {
+                    // si c'est un type particulier on ajoute le suffixe
+                    String description = champ.get("label").asText();
+                    if (suffixeFound != null) {
+                        description = description + " - " + suffixeFound;
+                    }
+                    return description;
+                }
+            }
+        }
+        // si on a rien, on va chercher du côté des tableaux
+        List<Map.Entry<String, JsonNode>> tableauWithTitle = new ArrayList<>();
+        extractTableauNodesWithParents(sections, null, tableauWithTitle);
+        for (Map.Entry<String, JsonNode> entry : tableauWithTitle) {
+            String title = "Tableau " + entry.getKey();
+            JsonNode tableau = entry.getValue();
+            // on regarde d'abord si c'est le chemin racine du tableau
+            if (tableau.get("path").asText().equals(modifiedModelPath)) {
+                return title;
+            }
+            for (JsonNode column : tableau.get("columns")) {
+                if (column.get("path").asText()
+                        .equals(modifiedModelPath.substring(modifiedModelPath.lastIndexOf('.') + 1))) {
+                    // si c'est un type particulier on ajoute le suffixe
+                    String description = column.get("label").asText();
+                    if (suffixeFound != null) {
+                        description = description + " - " + suffixeFound;
+                    }
+                    return title + " - " + description;
+                }
+            }
+
+        }
+
+        return null;
+    }
+
+    private static void extractTableauNodesWithParents(JsonNode node, String parentTitle,
+            List<Map.Entry<String, JsonNode>> tableauWithTitle) {
+        if (node.isObject()) {
+            JsonNode typeNode = node.get("type");
+            if (typeNode != null && "tableau".equals(typeNode.asText())) {
+                // On suppose que le titre est dans le nœud parent direct
+                tableauWithTitle.add(Map.entry(parentTitle, node));
+            }
+
+            JsonNode titreNode = node.get("titre");
+            String currentTitle = (titreNode != null) ? titreNode.asText() : parentTitle;
+
+            // Parcourir les enfants de l'objet
+            node.fields().forEachRemaining(
+                    entry -> extractTableauNodesWithParents(entry.getValue(), currentTitle, tableauWithTitle));
+        } else if (node.isArray()) {
+            // Si le nœud est un tableau
+            for (int i = 0; i < node.size(); i++) {
+                extractTableauNodesWithParents(node.get(i), parentTitle, tableauWithTitle);
+            }
+        }
+    }
+
 
     private String pathToCamelCase(String input) {
         if (input == null || input.isEmpty()) {

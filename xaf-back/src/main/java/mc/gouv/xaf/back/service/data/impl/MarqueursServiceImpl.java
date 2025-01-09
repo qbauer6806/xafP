7d3a16b56b6c532e60749d1369cebaf8495a9f5b
Ugo EL KOUBI <uelkoubi.ext@gouv.mc>
2025-01-09 15:33:58 +0100

    #66421 - Améliorations xaf 12 - fix trad tableau adresse pour description sur les vieux récaps

--- xaf-back/src/main/java/mc/gouv/xaf/back/service/data/impl/MarqueursServiceImpl.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/service/data/impl/MarqueursServiceImpl.java
@@ -168,7 +168,9 @@ public class MarqueursServiceImpl implements MarqueursService {
                 return title;
             }
             for (JsonNode column : tableau.get("columns")) {
-                if (column.get("path").asText()
+                String path = column.get("path").asText();
+                // on enlève Ligne1 à la fin s'il est présent, car sur des vieux récaps le path de l'adresse contient ce suffixe
+                if (path.replaceAll("Ligne1$", "")
                         .equals(modifiedModelPath.substring(modifiedModelPath.lastIndexOf('.') + 1))) {
                     // si c'est un type particulier on ajoute le suffixe
                     String description = column.get("label").asText();

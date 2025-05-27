151562c521a02151ee4a81fc4a602c06dc3b1a2c
Decool <xdecool.ext@gouv.mc>
2025-05-27 15:53:45 +0200

    refs #62921 - Ajout dd'une generated value dans le BO d'une opération

--- xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/data/entity/CommandeOperationBO.java
+++ xaf-back-paiement/src/main/java/mc/gouv/xaf/back/paiement/data/entity/CommandeOperationBO.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,6 +24,7 @@ import mc.gouv.xaf.back.paiement.data.enums.OperationTypeEnum;
 public class CommandeOperationBO {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "PK_OPERATIONS", nullable = false)
     private String pkOperations;
 

a6538a1446c2a50a952ad2063501cfac6cd3e956
Decool <xdecool.ext@gouv.mc>
2025-09-30 16:16:04 +0200

    [TECH] Ajout d'une erreur volontaire pour gestion erreur

--- xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/service/itg/resid/impl/ResidApiServiceImpl.java
+++ xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/service/itg/resid/impl/ResidApiServiceImpl.java
@@ -82,7 +82,7 @@ public class ResidApiServiceImpl implements ResidApiService {
     public static final String RESID_USAGERS_PATH = "/usagers";
     public static final String RESID_NPDHL_PATH = "/npdhl";
     public static final String RESID_ETAT_CAISSE_PATH = "/caisse/ouverture";
-    public static final String RESID_RETOUR_DEBIT_PATH = "/paiement/retourDebit";
+    public static final String RESID_RETOUR_DEBIT_PATH = "/paiement/retourDebi";
 
     public static final String LAST_SUCCESSFUL_SYNCHRO_KEY = "LAST_SUCCESSFUL_SYNCHRO";
 

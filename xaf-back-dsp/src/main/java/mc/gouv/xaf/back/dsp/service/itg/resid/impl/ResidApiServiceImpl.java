9571b073308e05b329b8f30c45babf4611dae8e4
Decool <xdecool.ext@gouv.mc>
2025-10-02 15:21:30 +0200

    refs #69816 - gestion d'incident en cas d'erreur au moment du rattrapage d'un débit

--- xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/service/itg/resid/impl/ResidApiServiceImpl.java
+++ xaf-back-dsp/src/main/java/mc/gouv/xaf/back/dsp/service/itg/resid/impl/ResidApiServiceImpl.java
@@ -81,7 +81,7 @@ public class ResidApiServiceImpl implements ResidApiService {
     public static final String RESID_USAGERS_PATH = "/usagers";
     public static final String RESID_NPDHL_PATH = "/npdhl";
     public static final String RESID_ETAT_CAISSE_PATH = "/caisse/ouverture";
-    public static final String RESID_RETOUR_DEBIT_PATH = "/paiement/retourDebi";
+    public static final String RESID_RETOUR_DEBIT_PATH = "/paiement/retourDebit";
 
     @Autowired
     private GouvPropertiesResolver gouvPropertiesResolver;

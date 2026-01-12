51258d6e3e7c53f7fe925dff50464a6a299e90dd
Decool <xdecool.ext@gouv.mc>
2026-01-12 09:35:03 +0100

    refs #75882 - [BO][PROD] Impossible d'accéder à l'onglet suppression avec 700 demandes

--- xaf-back/src/main/java/mc/gouv/xaf/back/data/dao/StatistiquesRepository.java
+++ xaf-back/src/main/java/mc/gouv/xaf/back/data/dao/StatistiquesRepository.java
@@ -19,10 +19,7 @@ public interface StatistiquesRepository extends CrudRepository<StatistiqueBO, In
     @Query("SELECT new mc.gouv.xaf.back.data.model.StatistiqueSubsetDTO(s2.identifiantDemande, s2.statutPublic, s2.date,s1.date) "
             + "FROM StatistiqueBO s1 " + "JOIN StatistiqueBO s2 ON s1.demandeId = s2.demandeId "
             + "WHERE s1.statutPublic = 'SUPPRIMEE' " + "AND s1.date between :startDate AND :endDate "
-            + "AND s2.statutPublic IN :statutValideOuRefuse "
-            //#59845 : On ajoute une sous requête pour récupérer la ligne la plus récente en cas de doublon
-            + "AND s2.pkStatistiques = (SELECT MAX (s3.pkStatistiques) FROM StatistiqueBO s3 WHERE s2.demandeId = s3.demandeId "
-            + "AND s3.statutPublic IN :statutValideOuRefuse)")
+            + "AND s2.statutPublic IN :statutValideOuRefuse")
     List<StatistiqueSubsetDTO> findAllBetweenDates(@Param("statutValideOuRefuse") List<String> statutValideOuRefuse,
             @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
